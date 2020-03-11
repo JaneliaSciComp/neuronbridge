6c2a10f36eb9b28d0d9260aa6fe5ccf84ee2e470
Jody Clements <jody@neomorphic.co.uk>
2020-03-10 21:39:16 -0400

    Removes AppliedRoute component
    This route is not necessary as it simply passes all the props to Route,
    so just use that instead.

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,18 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import AppliedRoute from "./components/AppliedRoute";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
-import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import Landing from "./containers/Landing";
-import NewNote from "./containers/NewNote";
-import EditNote from "./containers/EditNote";
 import SearchTabs from "./containers/SearchTabs";
 import About from "./containers/About";
 
 export default function Routes({ appProps }) {
   return (
     <Switch>
-      <AppliedRoute path="/" exact component={Landing} appProps={appProps} />
+      <Route path="/" exact component={Landing} appProps={appProps} />
       <UnauthenticatedRoute path="/login" exact component={Login} appProps={appProps} />
       <UnauthenticatedRoute path="/signup" exact component={Signup} appProps={appProps} />
       <AuthenticatedRoute path="/search/lines/:elemId" component={SearchTabs} appProps={appProps} />
