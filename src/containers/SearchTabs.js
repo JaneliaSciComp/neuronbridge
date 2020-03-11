43612df71f66cde864528a4bb2da4cf70c3c8280
Jody Clements <jody@neomorphic.co.uk>
2020-03-10 22:06:11 -0400

    Removes unused code and reformats.

--- src/containers/SearchTabs.js
+++ src/containers/SearchTabs.js
@@ -1,51 +1,48 @@
-import React, {useRef, useState, useEffect, useImperativeHandle} from "react";
-import {Input} from "antd";
-import { Tabs } from 'antd';
+import React from "react";
+import { Tabs } from "antd";
 import SearchSkeletons from "./SearchSkeletons";
 import SearchLines from "./SearchLines";
-import { Switch, useLocation, useParams, useHistory } from 'react-router-dom';
+import { useLocation, useParams } from "react-router-dom";
 import EntryList2 from "./EntryList2";
 import MyContext from "./MyContext";
 import Matches from "./Matches";
 
+const { TabPane } = Tabs;
+
 function SearchTabs(props) {
   let location = useLocation();
   let params = useParams();
-  let history = useHistory();
-  const { TabPane } = Tabs;
-  const { Search } = Input;
+  const { searchTerm } = params;
 
   let defaultKey = "1";
-
-  let elemId = params.elemId;
-  if (!elemId) {
-    elemId = null;
+  if (location.pathname.includes("skeleton")) {
+    defaultKey = "2";
+  } else if (location.pathname.includes("match")) {
+    defaultKey = "3";
   }
 
-  if (location.pathname.includes('skeleton')) {
-    defaultKey="2";
-  } else if (location.pathname.includes('match')) {
-    defaultKey="3";
-  }
-
-return (
+  return (
     <MyContext.Consumer>
-        {context => (
-          <div className="card-container">
-            <Tabs type="card" defaultActiveKey={defaultKey} onChange={context.changeTab}>
-              <TabPane tab="Published LM Lines" key="1" >
-                 <SearchLines elemId={ elemId } />
-              </TabPane>
-              <TabPane tab="Published EM Skeletons" key="2">
-                 <SearchSkeletons elemId={ elemId } />
-              </TabPane>
-              <TabPane tab="Matches" key="3">
-                 <Matches />
-              </TabPane>
-            </Tabs>
-            { context.result && <EntryList2 result={ context.result } /> }
-          </div>
-        )}
+      {context => (
+        <div className="card-container">
+          <Tabs
+            type="card"
+            defaultActiveKey={defaultKey}
+            onChange={context.changeTab}
+          >
+            <TabPane tab="Published LM Lines" key="1">
+              <SearchLines searchTerm={searchTerm} />
+            </TabPane>
+            <TabPane tab="Published EM Skeletons" key="2">
+              <SearchSkeletons searchTerm={searchTerm} />
+            </TabPane>
+            <TabPane tab="Matches" key="3">
+              <Matches />
+            </TabPane>
+          </Tabs>
+          {context.result && <EntryList2 result={context.result} />}
+        </div>
+      )}
     </MyContext.Consumer>
   );
 }
