b8e68a751e038ca18c439357854a1c099f55b6af
Jody Clements <jody@neomorphic.co.uk>
2020-03-10 21:27:43 -0400

    Formatting changes

--- src/containers/Matches.js
+++ src/containers/Matches.js
@@ -1,23 +1,22 @@
-import React, {useEffect} from "react";
-import { List, Avatar, Icon, Skeleton, Col, Row} from "antd";
-import Button from '@material-ui/core/Button';
+import React, { useEffect } from "react";
+import { List, Avatar, Icon, Skeleton, Col, Row } from "antd";
+import Button from "@material-ui/core/Button";
 import config from "../config";
 
 const IconText = ({ type, text }) => (
   <span>
     <Icon type={type} style={{ marginRight: 8 }} />
-    { text }
+    {text}
   </span>
 );
 
 export default function Matches(props) {
-
   const [open, setOpen] = React.useState(false);
   const [searchType, setSearchType] = React.useState(props.searchType);
   const [matches, setMatches] = React.useState(props.result);
 
   let keys = [];
-  const testId = '2711777429277376523';
+  const testId = "2711777429277376523";
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -27,27 +26,27 @@ export default function Matches(props) {
     setOpen(false);
   };
 
-  if (searchType == 'skeleton') {
-    keys = ['Body Id', 'Library']
-  }
-  else {
-    keys = ['Line', 'Slide Code'];
+  if (searchType == "skeleton") {
+    keys = ["Body Id", "Library"];
+  } else {
+    keys = ["Line", "Slide Code"];
   }
 
-  const getMatches = (event) => {
-      if (testId) {
-        const path = config.MATCH_PATH + testId + '.json';
-        fetch(path)
-          .then(function(response) {
-            return response.json();
-          })
-          .then(function(json) {
-            const matches = json['results'];
-            setMatches(matches)
-          }).catch(function(error) {
-            console.log(error);
-          });
-      }
+  const getMatches = event => {
+    if (testId) {
+      const path = config.MATCH_PATH + testId + ".json";
+      fetch(path)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(json) {
+          const matches = json["results"];
+          setMatches(matches);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
   };
 
   useEffect(() => {
@@ -55,52 +54,51 @@ export default function Matches(props) {
   });
 
   return (
-          <div>
-            <List
-              itemLayout="vertical"
-              size="small"
-              pagination={{
-                onChange: page => {
-                  console.log(page);
-                },
-                pageSize: 20,
-              }}
-              dataSource={ matches }
-              renderItem={ item => (
-                  <List.Item
-                    extra={
-                        <Button >
-                          <img
-                            width={500}
-                            alt="mip"
-                            src={ item.thumbnail_path }
-                          />
-                        </Button>
-                    }>
-                    <Skeleton loading={item.loading}>
-                    <Row>
-                      <Col>
-                        <List
-                            itemLayout="vertical"
-                            size="small"
-                            dataSource={ Object.keys(item.attrs) }
-                            renderItem={ subItem => (
-                              <List.Item>
-                                <Skeleton loading={item.loading} active>
-                                  <Row>
-                                    <Col>
-                                      <strong>{subItem}: </strong>{item[subItem]}
-                                    </Col>
-                                  </Row>
-                                </Skeleton>
-                              </List.Item>
-                            )}
-                        />
-                      </Col>
-                    </Row>
-                    </Skeleton>
-                  </List.Item>
-              )}
-            />
-          </div>)
+    <div>
+      <List
+        itemLayout="vertical"
+        size="small"
+        pagination={{
+          onChange: page => {
+            console.log(page);
+          },
+          pageSize: 20
+        }}
+        dataSource={matches}
+        renderItem={item => (
+          <List.Item
+            extra={
+              <Button>
+                <img width={500} alt="mip" src={item.thumbnail_path} />
+              </Button>
+            }
+          >
+            <Skeleton loading={item.loading}>
+              <Row>
+                <Col>
+                  <List
+                    itemLayout="vertical"
+                    size="small"
+                    dataSource={Object.keys(item.attrs)}
+                    renderItem={subItem => (
+                      <List.Item>
+                        <Skeleton loading={item.loading} active>
+                          <Row>
+                            <Col>
+                              <strong>{subItem}: </strong>
+                              {item[subItem]}
+                            </Col>
+                          </Row>
+                        </Skeleton>
+                      </List.Item>
+                    )}
+                  />
+                </Col>
+              </Row>
+            </Skeleton>
+          </List.Item>
+        )}
+      />
+    </div>
+  );
 }
--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -34,9 +34,9 @@ export default function Signup(props) {
 
   async function handleSubmit(event) {
     event.preventDefault();
-  
+
     setIsLoading(true);
-  
+
     try {
       const newUser = await Auth.signUp({
         username: fields.email,
@@ -44,26 +44,24 @@ export default function Signup(props) {
       });
       setIsLoading(false);
       setNewUser(newUser);
-    } 
-    catch (e) {
+    } catch (e) {
       alert(e.message);
       setIsLoading(false);
     }
   }
-  
+
   async function handleConfirmationSubmit(event) {
     event.preventDefault();
-  
+
     setIsLoading(true);
-  
+
     try {
       await Auth.confirmSignUp(fields.email, fields.confirmationCode);
       await Auth.signIn(fields.email, fields.password);
-  
+
       props.userHasAuthenticated(true);
       props.history.push("/");
-    } 
-    catch (e) {
+    } catch (e) {
       alert(e.message);
       setIsLoading(false);
     }
