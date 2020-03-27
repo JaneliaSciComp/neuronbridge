02c2061f117f07c0f36cfa67f5308dcfc2af88a2
Jody Clements <jody@neomorphic.co.uk>
2020-03-27 15:59:29 -0400

    Fixes typography in Landing component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,17 +1,25 @@
 // Landing.jsx
 import React from "react";
 import PropTypes from "prop-types";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Typography } from "antd";
 import SearchInput from "./SearchInput";
-import "antd/dist/antd.css";
+
+const { Title, Paragraph } = Typography;
 
 function Landing(props) {
   const { isAuthenticated } = props;
   return (
     <>
       {isAuthenticated && <SearchInput />}
-      <h1>Welcome to NeuronBridge</h1>
-    {!isAuthenticated && <p>Please <Link to="/login">login</Link> or <Link to="signup">sign up</Link> to start searching.</p>}
+      <Title>Welcome to NeuronBridge</Title>
+      {!isAuthenticated && (
+        <Paragraph strong>
+          Please <Link to="/login">login</Link> or{" "}
+          <Link to="signup">sign up</Link> to start searching.
+        </Paragraph>
+      )}
+      <Paragraph>Content Place holder about the site.</Paragraph>
     </>
   );
 }
