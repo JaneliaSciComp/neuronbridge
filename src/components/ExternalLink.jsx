a0d59251f6f90ee0f03ce9e06587335ec6fc11d1
Jody Clements <jody@neomorphic.co.uk>
2020-04-01 10:05:01 -0400

    Adds external link icon.

--- src/components/ExternalLink.jsx
+++ src/components/ExternalLink.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { faExternalLink } from "@fortawesome/pro-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const lmUrl = 'http://splitgal4.janelia.org/cgi-bin/view_splitgal4_imagery.cgi?line=<NAME>';
-const emUrl = 'https://neuprint.janelia.org/view?bodyid=<NAME>';
+const lmUrl =
+  "http://splitgal4.janelia.org/cgi-bin/view_splitgal4_imagery.cgi?line=<NAME>";
+const emUrl = "https://neuprint.janelia.org/view?bodyid=<NAME>";
 
-
-export default function ExternalLink({publishedName, isLM}) {
+export default function ExternalLink({ publishedName, isLM }) {
   if (isLM) {
-    return <a href={lmUrl.replace(/<NAME>/, publishedName)}>{publishedName}</a>
+    return (
+      <a href={lmUrl.replace(/<NAME>/, publishedName)}>
+        {publishedName} <FontAwesomeIcon icon={faExternalLink} size="xs" transform="up-10" />
+      </a>
+    );
   }
-  return <a href={emUrl.replace(/<NAME>/, publishedName)}>{publishedName}</a>
+  return (
+    <a href={emUrl.replace(/<NAME>/, publishedName)}>
+      {publishedName} <FontAwesomeIcon icon={faExternalLink} size="xs" transform="up-10" />
+    </a>
+  );
 }
 
 ExternalLink.propTypes = {
