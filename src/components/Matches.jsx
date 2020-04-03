b28ddff1a61c6e3fab482a926f00e1d01cfe1a3a
Jody Clements <jody@neomorphic.co.uk>
2020-04-03 16:23:49 -0400

    Adds results per line limit.
    There can be a lot of matches per line, based on slide code and channel.
    This update limits the number of results based on the number provided
    by the user.

--- src/components/Matches.jsx
+++ src/components/Matches.jsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useState, useContext } from "react";
 import PropTypes from "prop-types";
 import { useParams } from "react-router-dom";
-import { Switch, Row, Col, Pagination, Divider, Spin, message } from "antd";
+import {
+  Switch,
+  Row,
+  Col,
+  Pagination,
+  Divider,
+  Spin,
+  InputNumber,
+  message
+} from "antd";
 import { AppContext } from "../containers/AppContext";
 import LineSummary from "./LineSummary";
 import MatchSummary from "./MatchSummary";
@@ -21,6 +30,7 @@ export default function Matches(props) {
   const [modalOpen, setModalOpen] = useState(0);
   const [isLoading, setLoading] = useState(false);
   const [appState, setAppState] = useContext(AppContext);
+  const [resultsPerLine, setResultsPerLine] = useState(2);
 
   useEffect(() => {
     function getMatches() {
@@ -43,6 +53,10 @@ export default function Matches(props) {
 
   function handlePageChange(newPage) {
     setPage(newPage);
+  }
+
+  function handleResultsPerLine(count) {
+    setResultsPerLine(count);
   }
 
   function handleModalOpen(index) {
@@ -68,9 +82,52 @@ export default function Matches(props) {
     );
 
   if (matchMeta) {
-    fullList = matchMeta.results.sort((a, b) => {
-      return b.attrs["Matched pixels"] - a.attrs["Matched pixels"];
-    });
+    // if isLM then a more complex sort is required.
+    // - convert fullList into a list of lines where each line name
+    //   has the max score and  all the channels as an array of children
+    //   [
+    //     {
+    //       PublishedName: String,
+    //       Channels: [{},{},{}],
+    //       Score: Int <Max Score>
+    //     },
+    //     {...}
+    //   ]
+    //
+    if (searchType !== "lines") {
+      const byLines = {};
+      matchMeta.results.forEach(result => {
+        const publishedName = result.attrs["Published Name"];
+        const currentScore = result.attrs["Matched pixels"];
+
+        if (publishedName in byLines) {
+          byLines[publishedName].score = Math.max(
+            parseInt(byLines[publishedName].score, 10),
+            currentScore
+          );
+          byLines[publishedName].channels.push(result);
+        } else {
+          byLines[publishedName] = {
+            score: parseInt(currentScore, 10),
+            channels: [result]
+          };
+        }
+      });
+      const sortedByLine = Object.values(byLines).sort(
+        (a, b) => b.score - a.score
+      );
+      const limitedByLineCount = sortedByLine.map(line =>
+        line.channels
+          .sort((a, b) => b.attrs["Matched pixels"] - a.attrs["Matched pixels"])
+          .slice(0, resultsPerLine)
+      );
+      fullList = [].concat(...limitedByLineCount);
+    } else {
+      fullList = matchMeta.results.sort((a, b) => {
+        return b.attrs["Matched pixels"] - a.attrs["Matched pixels"];
+      });
+    }
+
     pageinatedList = fullList.slice(
       page * matchesPerPage - matchesPerPage,
       page * matchesPerPage
@@ -106,22 +163,37 @@ export default function Matches(props) {
       )}
       {!isLoading && matchMeta && (
         <>
-          <Row style={{paddingBottom: "1em"}}>
-            <Col span={4}>
-              <h3>{(searchType === "lines") ? 'LM to EM' : 'EM to LM'} Matches</h3>
+          <Row style={{ paddingBottom: "1em" }}>
+            <Col span={3}>
+              <h3>
+                {searchType === "lines" ? "LM to EM" : "EM to LM"} Matches
+              </h3>
             </Col>
-            <Col span={16} style={{ textAlign: "center" }}>
+            <Col span={14} style={{ textAlign: "center" }}>
               <Pagination
                 current={page}
                 pageSize={matchesPerPage}
                 onChange={handlePageChange}
-                total={matchMeta.results.length}
+                total={fullList.length}
                 showTotal={(total, range) =>
                   `${range[0]}-${range[1]} of ${total} matches`
                 }
               />
             </Col>
             <Col span={4} style={{ textAlign: "right" }}>
+              {searchType !== "lines" && (
+                <p>
+                  <InputNumber
+                    min={1}
+                    max={100}
+                    value={resultsPerLine}
+                    onChange={handleResultsPerLine}
+                  />{" "}
+                  results per line
+                </p>
+              )}
+            </Col>
+            <Col span={2} style={{ textAlign: "right" }}>
               <Switch
                 checked={appState.gridView}
                 checkedChildren="Grid"
