5976e2a5128db4cc20e2235a6f0dfb666c244a5d
Jody Clements <jody@neomorphic.co.uk>
2020-04-22 14:16:42 -0400

    Adds media queries to improve homepage image
    The background image on the home page was not positioned well, when the
    screen size was reduced to something used on a cell phone. This fixes
    the issue, by adding media queries to change the image to something more
    subdued, so that the text can be read when on top of it.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -15,63 +15,67 @@ function Landing(props) {
       {isAuthenticated && <SearchInput />}
       <div className="landing">
         <Title>Welcome to NeuronBridge</Title>
-        <div className="copy">
-          {!isAuthenticated && (
-            <Paragraph strong>
-              Please <Link to="/login">login</Link> or{" "}
-              <Link to="signup">sign up</Link> to start searching.
-            </Paragraph>
-          )}
-          <Paragraph>Find your neuron fast!</Paragraph>
+        <Row>
+          <Col md={12} className="copy">
+            {!isAuthenticated && (
+              <Paragraph strong>
+                Please <Link to="/login">login</Link> or{" "}
+                <Link to="signup">sign up</Link> to start searching.
+              </Paragraph>
+            )}
+            <Paragraph>Find your neuron fast!</Paragraph>
 
-          <Paragraph>
-            Search light and electron microscopy data sets of the Drosophila
-            nervous system provided by the{" "}
-            <a href="https://www.janelia.org/project-team/flylight">FlyLight</a>{" "}
-            and <a href="https://www.janelia.org/project-team/flyem">FlyEM</a>{" "}
-            projects at{" "}
-            <a href="https://www.janelia.org">Janelia Research Campus</a>. You
-            can find similar neurons based on shape regardless of data set.
-          </Paragraph>
-
-          {!isAuthenticated && (
             <Paragraph>
-              Please login above if you already know your EM body ID or GAL4
-              line name, or browse our image collections at:
+              Search light and electron microscopy data sets of the Drosophila
+              nervous system provided by the{" "}
+              <a href="https://www.janelia.org/project-team/flylight">
+                FlyLight
+              </a>{" "}
+              and <a href="https://www.janelia.org/project-team/flyem">FlyEM</a>{" "}
+              projects at{" "}
+              <a href="https://www.janelia.org">Janelia Research Campus</a>. You
+              can find similar neurons based on shape regardless of data set.
             </Paragraph>
-          )}
-          {isAuthenticated && (
+
+            {!isAuthenticated && (
+              <Paragraph>
+                Please login above if you already know your EM body ID or GAL4
+                line name, or browse our image collections at:
+              </Paragraph>
+            )}
+            {isAuthenticated && (
+              <Paragraph>
+                Start your search above if you already know your EM body ID or
+                GAL4 line name, or browse our image collections at:
+              </Paragraph>
+            )}
+
+            <Row className="collections">
+              <Col span={12}>
+                <a href="https://neuprint.janelia.org/">FlyEM Hemibrain</a>
+              </Col>
+              <Col span={12}>
+                <a href="http://splitgal4.janelia.org">FlyLight Split-GAL4</a>
+              </Col>
+              <Col span={12}>
+                <p>FlyLight Generation 1 MCFO (coming soon)</p>
+              </Col>
+              <Col span={12}>
+                <p>FlyLight Generation 1 GAL4 screen (coming soon)</p>
+              </Col>
+            </Row>
+
             <Paragraph>
-              Start your search above if you already know your EM body ID or
-              GAL4 line name, or browse our image collections at:
+              We offer fast color depth mask searching, with no segmentation
+              needed (see About page). For NBLAST searching, try Virtual Fly
+              Brain.
             </Paragraph>
-          )}
 
-          <Row className="collections">
-            <Col span={12}>
-              <a href="https://neuprint.janelia.org/">FlyEM Hemibrain</a>
-            </Col>
-            <Col span={12}>
-              <a href="http://splitgal4.janelia.org">FlyLight Split-GAL4</a>
-            </Col>
-            <Col span={12}>
-              <p>FlyLight Generation 1 MCFO (coming soon)</p>
-            </Col>
-            <Col span={12}>
-              <p>FlyLight Generation 1 GAL4 screen (coming soon)</p>
-            </Col>
-          </Row>
-
-          <Paragraph>
-            We offer fast color depth mask searching, with no segmentation
-            needed (see About page). For NBLAST searching, try Virtual Fly
-            Brain.
-          </Paragraph>
-
-          <Paragraph>
-            <Link to="/about">Learn more…</Link>
-          </Paragraph>
-        </div>
+            <Paragraph>
+              <Link to="/about">Learn more…</Link>
+            </Paragraph>
+          </Col>
+        </Row>
       </div>
     </>
   );
