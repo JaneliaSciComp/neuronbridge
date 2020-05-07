c3478e6cb490fa6e95426d2f2e039fc5ce1599a3
Jody Clements <jody@neomorphic.co.uk>
2020-05-07 09:45:32 -0400

    Expands search help & option to disable scroll
    The help contents scroll to the ref found in AppContext, unless the
    scroll is disabled. This is primarily used when the contents are embedded
    on the help page and not in the pop up.

--- src/components/HelpContents.jsx
+++ src/components/HelpContents.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
 import { InputNumber } from "antd";
+import SearchInput from "./SearchInput";
 import { FilterContext } from "../containers/FilterContext";
 import { AppContext } from "../containers/AppContext";
 
-export default function HelpPage() {
+export default function HelpContents({ scroll }) {
   const [filterState, setFilterState] = useContext(FilterContext);
   const [appState] = useContext(AppContext);
 
@@ -11,15 +13,18 @@ export default function HelpPage() {
 
   const refLookup = {
     MatchesEMtoLM: useRef(),
+    MatchesLMtoEM: useRef(),
     SearchInput: useRef()
   };
 
   // use Effect to scroll to target set in the appState?
   useEffect(() => {
-    if (refLookup[appState.helpTarget]) {
-      if (refLookup[appState.helpTarget].current) {
-        helpContentRef.current.parentElement.scrollTop =
-          refLookup[appState.helpTarget].current.offsetTop - 60;
+    if (scroll) {
+      if (refLookup[appState.helpTarget]) {
+        if (refLookup[appState.helpTarget].current) {
+          helpContentRef.current.parentElement.scrollTop =
+            refLookup[appState.helpTarget].current.offsetTop - 60;
+        }
       }
     }
   }, [appState.helpTarget, refLookup]);
@@ -30,14 +35,37 @@ export default function HelpPage() {
 
   return (
     <div ref={helpContentRef}>
-      <h3 ref={refLookup.SearchInput}>Searching:</h3>
+      <h2 ref={refLookup.SearchInput}>
+        Searching:
+      </h2>
       <p>
-        At the moment, searching is limited to a case sensitive PREFIX search. This means that
-        your query string will only match the beginning of the body Id or Line
-        Name that you are searching for and only match if the case is identical.
+        Searching for your EM body id or cell line of interest should be easy.
+        To that end, We setup the search input to default to an exact match with
+        optional wild cards. For example, if you were looking for the cell line
+        LH173, searching for &ldquo;LH173&rdquo; would find exactly one result
+        for that cell line.
       </p>
-      <h3>LM to EM Matches:</h3>
-      <h3 ref={refLookup.MatchesEMtoLM}>EM to LM Matches:</h3>
+
+      <SearchInput examples={false} searchTerm="LH173" help={false} />
+
+      <p>
+        If you were interested in looking at all the cell lines from our 2014
+        mushroom body paper, then you could search with a wild card like
+        &ldquo;MB*&rdquo;.
+      </p>
+
+      <SearchInput examples={false} searchTerm="MB*" help={false} />
+
+      <p>
+        Please note wild card searches are limited to the first 100 matches. To
+        see more entries, please enter a more specific search term, eg:
+        &ldquo;MB11*&rdquo;
+      </p>
+
+      <SearchInput examples={false} searchTerm="MB11*" help={false} />
+
+      <h2 ref={refLookup.MatchesLMtoEM}>LM to EM Matches:</h2>
+      <h2 ref={refLookup.MatchesEMtoLM}>EM to LM Matches:</h2>
       <p>
         All matching images in a line are sorted together by the highest scoring
         image in that line. By default, we display a single image per line. This
@@ -57,3 +85,11 @@ export default function HelpPage() {
     </div>
   );
 }
+
+HelpContents.propTypes = {
+  scroll: PropTypes.bool
+};
+
+HelpContents.defaultProps = {
+  scroll: true
+};
