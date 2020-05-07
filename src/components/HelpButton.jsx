fca42758b29f848a108dd9fc0edbc73bcc9bf1bb
Jody Clements <jody@neomorphic.co.uk>
2020-05-07 09:43:46 -0400

    Adds option to make help button a text link.
    This makes it possible to embed a link to the help page in text copy on
    the site.

--- src/components/HelpButton.jsx
+++ src/components/HelpButton.jsx
@@ -4,22 +4,34 @@ import { Button } from "antd";
 import { QuestionOutlined } from "@ant-design/icons";
 import { AppContext } from "../containers/AppContext";
 
-export default function HelpButton({target}) {
+export default function HelpButton({target, text}) {
   const [appState, setAppState] = useContext(AppContext);
 
   function handleHelp() {
     setAppState({ ...appState, showHelp: true, helpTarget: target });
   }
+
+  if (text) {
+    return (
+      <Button type="link" style={{'padding': '0'}} onClick={handleHelp}>{text}</Button>
+    );
+  }
+
   return (
     <Button
       size="small"
       shape="circle"
       icon={<QuestionOutlined />}
       onClick={handleHelp}
-    />
+   />
   );
 }
 
 HelpButton.propTypes = {
-  target: PropTypes.string.isRequired
+  target: PropTypes.string.isRequired,
+  text: PropTypes.string
+};
+
+HelpButton.defaultProps = {
+  text: null
 };
