303b1fada68be064d3e520c42d5409ff153b2cba
Jody Clements <jody@neomorphic.co.uk>
2020-05-14 15:10:58 -0400

    Removes unused custom hook

--- src/libs/hooksLib.js
+++ src/libs/hooksLib.js
@@ -1,21 +1,6 @@
-import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 // eslint-disable-next-line
-export function useFormFields(initialState) {
-  const [fields, setValues] = useState(initialState);
-
-  return [
-    fields,
-    function(event) {
-      setValues({
-        ...fields,
-        [event.target.id]: event.target.value
-      });
-    }
-  ];
-}
-
 export function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
