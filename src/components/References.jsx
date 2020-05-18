cecf6bbb510681036f6ba693b7cd5b3813c043f0
Jody Clements <jody@neomorphic.co.uk>
2020-05-18 13:18:22 -0400

    Adds EM-LM unification template reference to about
    Also changes date formats to be uniform through the page.

--- src/components/References.jsx
+++ src/components/References.jsx
@@ -6,26 +6,26 @@ export default function References() {
     <dl>
       <dt>Gen1 MCFO collection</dt>
       <dd>
-        <span>Meissner et al., (2020)</span>
+        <span>Meissner et al., 2020</span>
       </dd>
 
       <dt>EM Hemibrain</dt>
       <dd>
-        <a href="https://doi.org/10.1101/2020.01.21.911859">Xu et al., (2020)</a>
+        <a href="https://doi.org/10.1101/2020.01.21.911859">Xu et al., 2020</a>
       </dd>
 
       <dt>Gen1 GAL4 collections</dt>
       <dd>
-        <a href="https://dx.doi.org/10.1016/j.celrep.2012.09.011">Jenett et al., (2012) for Janelia Gen1 GAL4</a>
+        <a href="https://dx.doi.org/10.1016/j.celrep.2012.09.011">Jenett et al., 2012; for Janelia Gen1 GAL4</a>
       </dd>
       <dd>
-        <a href="https://doi.org/10.1101/198648"> Tirian and Dickson, (2017) for VT Gen1 GAL4</a>
+        <a href="https://doi.org/10.1101/198648"> Tirian and Dickson, 2017; for VT Gen1 GAL4</a>
       </dd>
       <dd>
-        <a href="https://doi.org/10.1534/genetics.118.300682"> Dionne et al., (2018) for Janelia hemidrivers</a>
+        <a href="https://doi.org/10.1534/genetics.118.300682"> Dionne et al., 2018; for Janelia hemidrivers</a>
       </dd>
       <dd>
-        <a href="https://doi.org/10.1101/198648"> Tirian and Dickson (2017) for VT hemidrivers</a>
+        <a href="https://doi.org/10.1101/198648"> Tirian and Dickson, 2017; for VT hemidrivers</a>
       </dd>
 
       <dt>Split-GAL4 Lines</dt>
@@ -60,12 +60,17 @@ export default function References() {
         <a href="https://doi.org/10.1038/s41586-020-2055-9">Wang, et al., 2020</a>
       </dd>
 
+      <dt>EM-LM unification template</dt>
+        <dd>
+          <a href="https://doi.org/10.1101/376384">Bogovic et al., 2019</a>
+          </dd>
+
       <dt>NeuronBridge</dt>
       <dd>
-        <a href="https://doi.org/10.25378/janelia.12159378.v1">Clements et al., (2020)</a>
+        <a href="https://doi.org/10.25378/janelia.12159378.v1">Clements et al., 2020</a>
       </dd>
       <dd>
-        <a href="https://doi.org/10.1101/318006">Otsuna et al., (2018)</a>
+        <a href="https://doi.org/10.1101/318006">Otsuna et al., 2018</a>
       </dd>
     </dl>
   );
