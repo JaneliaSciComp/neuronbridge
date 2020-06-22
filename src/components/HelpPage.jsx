fec2fae94afd1991ee65a1c844a703068f9154f6
Jody Clements <jody@neomorphic.co.uk>
2020-06-22 10:34:19 -0400

    Adds tutorial video to the main help page.
    This video is not accessible from the pop up context at this time.

--- src/components/HelpPage.jsx
+++ src/components/HelpPage.jsx
@@ -8,6 +8,19 @@ export default function HelpPage() {
   return (
     <div>
       <Title>Help</Title>
+        <p>A quick tutorial video highlighting different aspects of neuronbridge.</p>
+        <div className="video">
+          <iframe
+            className="iframe"
+            title="Providing reconstruction feedback"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/Iw-w-O42ReE"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
       <HelpContents scroll={false} />
     </div>
   );
