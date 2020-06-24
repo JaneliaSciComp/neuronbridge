96a2d9a45c35ce6fac3f010127c222237764e311
Jody Clements <jody@neomorphic.co.uk>
2020-06-24 15:45:41 -0400

    Adds form for search upload meta data.

--- src/components/SearchUpload.jsx
+++ src/components/SearchUpload.jsx
@@ -4,6 +4,7 @@ import { faCloudUploadAlt } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Auth, Storage } from "aws-amplify";
 import SearchUploadMeta from "./SearchUploadMeta";
+import config from "../config";
 import "./SearchUpload.css";
 
 const { Dragger } = Upload;
@@ -15,14 +16,14 @@ export default function SearchUpload() {
     window.foo = upload;
 
     Auth.currentCredentials().then(() => {
-      Storage.put(upload.file.uid, upload.file, {
+      Storage.put(`${upload.file.uid}/${upload.file.uid}`, upload.file, {
         progressCallback: progress => {
           const percent = (progress.loaded * 100) / progress.total;
           upload.onProgress({ percent });
         },
         contentType: upload.file.type,
         level: "private",
-        bucket: "janelia-neuronbridge-searches-dev"
+        bucket: config.SEARCH_BUCKET
       })
         .then(result => {
           setUploadedNames(names => {
@@ -36,9 +37,9 @@ export default function SearchUpload() {
 
   function onRemove(file) {
     Auth.currentCredentials().then(() => {
-      Storage.remove(file.uid, {
+      Storage.remove(`${file.uid}/${file.uid}`, {
         level: "private",
-        bucket: "janelia-neuronbridge-searches-dev"
+        bucket: config.SEARCH_BUCKET
       })
         .then(() => {
           setUploadedNames(names => {
@@ -51,24 +52,22 @@ export default function SearchUpload() {
 
   return (
     <div className="uploader">
-      {uploadedNames.length === 0 && (
-        <Dragger
-          name="file"
-          action=""
-          withCredentials
-          listType="picture"
-          onRemove={onRemove}
-          customRequest={customRequest}
-          showUploadList
-        >
-          <p className="ant-upload-drag-icon">
-            <FontAwesomeIcon icon={faCloudUploadAlt} size="5x" />
-          </p>
-          <p className="ant-upload-text">
-            Click here or drag a file to this area to upload.
-          </p>
-        </Dragger>
-      )}
+      <Dragger
+        name="file"
+        action=""
+        withCredentials
+        listType="picture"
+        onRemove={onRemove}
+        customRequest={customRequest}
+        showUploadList
+      >
+        <p className="ant-upload-drag-icon">
+          <FontAwesomeIcon icon={faCloudUploadAlt} size="5x" />
+        </p>
+        <p className="ant-upload-text">
+          Click here or drag a file to this area to upload.
+        </p>
+      </Dragger>
       <SearchUploadMeta uploadedName={uploadedNames[0]} />
     </div>
   );
--- src/components/SearchUploadMeta.jsx
+++ src/components/SearchUploadMeta.jsx
@@ -1,21 +1,162 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Button } from "antd";
+import { Form, InputNumber, Select, Button, Switch } from "antd";
+import { CloseOutlined, CheckOutlined } from "@ant-design/icons";
+import { Auth, Storage } from "aws-amplify";
+import config from "../config";
 
-export default function SearchUploadMeta({uploadedName}) {
+const { Option } = Select;
 
-  if (!uploadedName) {
+export default function SearchUploadMeta({ uploadedName }) {
+  /* if (!uploadedName) {
     return null;
-  }
+  } */
+
+  const [isAligned, setIsAligned] = useState(true);
+
+  const onFinish = values => {
+    Auth.currentCredentials().then(() => {
+      Storage.put(`${uploadedName}.search`, values, {
+        level: "private",
+        bucket: config.SEARCH_BUCKET
+      })
+        .then(result => {
+          console.log(result);
+        })
+        .catch(e => console.log(e));
+    });
+  };
+
+  const onFinishFailed = error => {
+    console.log(error);
+  };
+
+  const onAlignedChange = checked => {
+    setIsAligned(checked);
+  };
 
   return (
     <div>
+      <p> Search parameters for {uploadedName}</p>
       <p>
-        Loop over uploaded names and display meta form for each. On submit,
-        upload the meta json to s3 for each uploaded image.
+        <Switch
+          checkedChildren={<CheckOutlined />}
+          unCheckedChildren={<CloseOutlined />}
+          checked={isAligned}
+          onChange={onAlignedChange}
+        />{" "}
+        Has this image been aligned already?
       </p>
-        <p>{uploadedName}</p>
-      <Button size="small">Search</Button>
+
+      <Form
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        name="basic"
+        initialValues={{ anatomicalregion: "brain", algorithm: "max" }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Search Type"
+          name="searchtype"
+          rules={[{ required: true, message: "Please choose a search type!" }]}
+        >
+          <Select>
+            <Option value="em2lm">EM to LM</Option>
+            <Option value="lm2em">LM to EM</Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item label="Template Matching Algorithm" name="algorithm">
+          <Select>
+            <Option value="max">
+              Maximum Intensity (Good for clean samples)
+            </Option>
+            <Option value="avg">
+              Average Intensity (Better for noisy samples)
+            </Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item
+          label="Image mime-type"
+          name="mimetype"
+          rules={[{ required: true, message: "Please choose a mime-type!" }]}
+        >
+          <Select>
+            <Option value="png">png</Option>
+            <Option value="jpeg">jpeg</Option>
+            <Option value="tiff">tiff</Option>
+            <Option value="lsm">lsm</Option>
+            <Option value="czi">czi</Option>
+            <Option value="oif">oif</Option>
+          </Select>
+        </Form.Item>
+
+        {!isAligned && (
+          <>
+            <Form.Item
+              label="Voxel Size (microns)"
+              rules={[
+                { required: true, message: "Please choose a voxel size!" }
+              ]}
+              style={{ marginBottom: 0 }}
+            >
+              <Form.Item
+                name="voxelx"
+                rules={[{ required: true }]}
+                style={{ display: "inline-block" }}
+              >
+                <InputNumber />
+              </Form.Item>
+              <span>x</span>
+              <Form.Item
+                name="voxely"
+                rules={[{ required: true }]}
+                style={{ display: "inline-block" }}
+              >
+                <InputNumber />
+              </Form.Item>
+              <span>x</span>
+              <Form.Item
+                name="voxelz"
+                rules={[{ required: true }]}
+                style={{ display: "inline-block" }}
+              >
+                <InputNumber />
+              </Form.Item>
+            </Form.Item>
+
+            <Form.Item
+              label="Channel to Align"
+              name="channel"
+              rules={[{ required: true, message: "Please choose a channel!" }]}
+            >
+              <Select>
+                <Option value="0">0</Option>
+                <Option value="1">1</Option>
+                <Option value="2">2</Option>
+                <Option value="3">3</Option>
+                <Option value="4">4</Option>
+                <Option value="5">5</Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item label="Anatomical Region" name="anatomicalregion">
+              <Select>
+                <Option value="brain">Brain</Option>
+                <Option value="vnc">VNC</Option>
+              </Select>
+            </Form.Item>
+          </>
+        )}
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
