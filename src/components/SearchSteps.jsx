a57cbb713a16221c2ecb7f0059f1fe57371c2508
Jody Clements <jody@neomorphic.co.uk>
2020-07-30 14:59:18 -0400

    Converts from material-ui steps to ant-d steps
    Not sure why the material-ui ones got in there in the first place, but
    using two different frameworks would have been a huge mistake.

--- src/components/SearchSteps.jsx
+++ src/components/SearchSteps.jsx
@@ -1,37 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
-import Stepper from "@material-ui/core/Stepper";
-import Step from "@material-ui/core/Step";
-import StepLabel from "@material-ui/core/StepLabel";
+import { Steps } from 'antd';
 
-const useStyles = makeStyles(() => ({
-  root: {
-    width: "100%"
-  },
-}));
+const {Step} = Steps;
 
 function getSteps() {
   return ["Files Uploaded", "Alignment", "Search", "Complete"];
 }
 
 export default function SearchSteps({ search }) {
-  const classes = useStyles();
-  // const [activeStep, setActiveStep] = React.useState(0);
   const steps = getSteps();
+  const { error } = search;
 
   return (
-    <div className={classes.root}>
-      <Stepper activeStep={search.step} alternativeLabel>
-        {steps.map(label => {
-          const { error } = search;
-          return (
-            <Step key={label}>
-              <StepLabel error={error}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
+    <div>
+        <Steps current={search.step} status={error}>
+          {steps.map(label => {
+            return (
+              <Step key={label} title={label} />
+            );
+          })}
+        </Steps>
     </div>
   );
 }
