e9ebc6ab52b20dd5836366d023204cf520e61e84
Jody Clements <jody@neomorphic.co.uk>
2020-08-07 10:14:51 -0400

    Adds link, on the help page, to download MIPs.

--- src/components/HelpPage.jsx
+++ src/components/HelpPage.jsx
@@ -8,19 +8,29 @@ export default function HelpPage() {
   return (
     <div>
       <Title>Help</Title>
-        <p>A quick tutorial video highlighting different aspects of neuronbridge.</p>
-        <div className="video">
-          <iframe
-            className="iframe"
-            title="Providing reconstruction feedback"
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/Iw-w-O42ReE"
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </div>
+      <p>
+        A quick tutorial video highlighting different aspects of neuronbridge.
+      </p>
+      <div className="video">
+        <iframe
+          className="iframe"
+          title="Providing reconstruction feedback"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/Iw-w-O42ReE"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+      <Title level={3}>Obtaining the raw data</Title>
+      <p>
+        Download the{" "}
+        <a href="https://www.janelia.org/open-science/color-depth-mip">
+          Color-Depth MIPs
+        </a>{" "}
+        for your own research.
+      </p>
       <HelpContents scroll={false} />
     </div>
   );
