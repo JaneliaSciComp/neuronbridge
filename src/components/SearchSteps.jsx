ee05cf7c9479dbafae16e331b95fe422ea7887ea
Jody Clements <jody@neomorphic.co.uk>
2020-08-18 15:15:33 -0400

    Updates search steps to show the correct step
    It was using the steps 0-4 as given to show the current step in the UI,
    but steps 1 & 2 and steps 3 & 4 are displayed as the same step, so
    needed to account for that.

--- src/components/SearchSteps.jsx
+++ src/components/SearchSteps.jsx
@@ -13,9 +13,25 @@ export default function SearchSteps({ search }) {
   const steps = getSteps();
   const { error } = search;
 
+  // there are 5 steps in the process stored in the databsae, but we
+  // only display 3 in this component, so need this replaces 2 of them
+  // with the step that we want to show.
+  let currentStep = 0;
+  switch(search.step) {
+    case 2:
+      currentStep = 1;
+      break;
+    case 3:
+    case 4:
+      currentStep = 2;
+      break;
+    default:
+      currentStep = search.step;
+  }
+
   return (
     <div className="searchSteps">
-        <Steps size="small" current={search.step} status={error}>
+        <Steps size="small" current={currentStep} status={error}>
           {steps.map(label => {
             return <Step key={label} title={label} />;
           })}
