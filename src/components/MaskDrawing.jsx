7bf74a81c1cc5da683907af82d52829b485941e8
Jody Clements <jody@neomorphic.co.uk>
2020-08-24 13:36:21 -0400

    Adds mask selection and upload.

--- src/components/MaskDrawing.jsx
+++ src/components/MaskDrawing.jsx
@@ -1,27 +1,144 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import PropTypes from "prop-types";
+import { Button } from "antd";
 import "./MaskDrawing.css";
 
-export default function MaskDrawing({ imgSrc }) {
+function getMousePos(evt) {
+  const canvas = evt.target;
+  const rect = canvas.getBoundingClientRect();
+  return [
+    ((evt.clientX - rect.left) * canvas.width) / canvas.clientWidth,
+    ((evt.clientY - rect.top) * canvas.height) / canvas.clientHeight
+  ];
+}
+
+const img = new Image();
+
+export default function MaskDrawing({ imgSrc, onMaskChange }) {
+  const [isDrawing, setDrawing] = useState(false);
+  const canvasRef = useRef(null);
+  const [pathStart, setPathStart] = useState([]);
+
+  const handleClearMask = () => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.clearRect(0, 0, 20000, 20000);
+      ctx.beginPath();
+      ctx.closePath();
+      onMaskChange(null);
+    }
+  };
+
+  useEffect(() => {
+    // fetch the input image and save it to the state
+     if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.clearRect(0, 0, 20000, 20000);
+      ctx.beginPath();
+      ctx.closePath();
+    }
+    img.src = imgSrc;
+  }, [imgSrc]);
+
   if (!imgSrc) {
     return (
       <div className="mdplaceholder">
         <h2>Choose a channel to create your mask</h2>
-        <img src="/maskplaceholder.jpg" alt="desaturated color depth mip placeholder" />
+        <img
+          src="/maskplaceholder.jpg"
+          alt="desaturated color depth mip placeholder"
+        />
       </div>
     );
   }
 
+  const handleCreateMask = () => {
+    // get the image data and serialize it
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.save();
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 20000, 20000);
+    ctx.clip();
+    ctx.drawImage(img, 0, 0);
+    ctx.restore();
+    canvasRef.current.toBlob(data => onMaskChange(data))
+  };
+
+  const handleMouseMove = e => {
+    if (isDrawing) {
+      const position = getMousePos(e);
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.lineTo(...position);
+      ctx.stroke();
+    }
+  };
+
+  const handleMouseUp = e => {
+    if (e.button === 0) {
+      setDrawing(false);
+      const position = getMousePos(e);
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.lineTo(...position);
+      ctx.stroke();
+
+      // close path
+      ctx.lineWidth = "2";
+      ctx.lineTo(pathStart[0], pathStart[1]);
+      ctx.stroke();
+      ctx.closePath();
+    }
+  };
+
+  const handleMouseDown = e => {
+    if (e.button === 0) {
+      setDrawing(true);
+      const position = getMousePos(e);
+      setPathStart(position);
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.clearRect(0, 0, 20000, 20000);
+      ctx.strokeStyle = "yellow";
+      ctx.lineWidth = "1";
+      ctx.beginPath();
+      ctx.moveTo(...position);
+    }
+  };
+
   return (
-    <div className="mdplaceholder">
-      <h2>Drawing canvas here for {imgSrc}</h2>
-      <img src="/maskplaceholder.jpg" alt="desaturated color depth mip placeholder" />
+    <div>
+      <div id="maskLassoContainer">
+        <canvas
+          ref={canvasRef}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onMouseMove={handleMouseMove}
+          id="imaskLassoCanvas"
+          style={{
+            position: "absolute",
+            left: 0,
+            top: 0,
+            zIndex: 1,
+            width: 1210,
+            height: 566
+          }}
+          width={1210}
+          height={566}
+        />
+        <img
+          src={imgSrc}
+          alt="desaturated color depth mip placeholder"
+        />
+      </div>
+      <Button type="primary" onClick={handleCreateMask}>
+        Create Mask
+      </Button>
+      <Button onClick={handleClearMask}>Clear Mask</Button>
     </div>
   );
 }
 
 MaskDrawing.propTypes = {
-  imgSrc: PropTypes.string
+  imgSrc: PropTypes.string,
+  onMaskChange: PropTypes.func.isRequired
 };
 
 MaskDrawing.defaultProps = {
--- src/components/MaskSelection.jsx
+++ src/components/MaskSelection.jsx
@@ -1,18 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
-import { API, graphqlOperation } from "aws-amplify";
-import { message, Button, Radio, Typography, Row, Col, Card } from "antd";
+import { Auth, Storage, API, graphqlOperation } from "aws-amplify";
+import {
+  message,
+  Button,
+  Radio,
+  Typography,
+  Row,
+  Col,
+  Card,
+  Divider
+} from "antd";
 import NotFound from "./NotFound";
 import MaskDrawing from "./MaskDrawing";
 import * as queries from "../graphql/queries";
 import { signedLink } from "../libs/awsLib";
+import config from "../config";
 
 const { Title, Paragraph } = Typography;
 
 export default function MaskSelection({ match }) {
   const searchId = match.params.id;
   const [searchMeta, setSearchMeta] = useState(null);
+  const [maskedImage, setMaskedImage] = useState(null);
   const [missingResults, setMissingResults] = useState(false);
   const [channel, setChannel] = useState(null);
   const history = useHistory();
@@ -29,7 +40,7 @@ export default function MaskSelection({ match }) {
           // When masks are ready this needs to be changed over.
           const uploadUrl = `${currentMeta.searchDir}/${currentMeta.upload}`;
           // TODO: add another step here to generate the real imageURL,
-          // rather than use the sameone as the thumbnail.
+          // rather than use the same one as the thumbnail.
           signedLink(uploadUrl).then(result => {
             const metaWithSignedUrls = {
               ...currentMeta,
@@ -63,9 +74,17 @@ export default function MaskSelection({ match }) {
     const title = `Channel ${image}`;
     const selectTitle = <Radio value={image}>{title}</Radio>;
     return (
-      <Col key={image} xs={24} md={12} lg={12} xl={8}>
-        <Card size="small" title={selectTitle} style={{ width: "100%", marginBottom: '1em' }}>
-          <img src="/maskplaceholder.jpg" style={{maxWidth: '100%'}} alt="colordepth mip" />
+      <Col key={image} xs={24} md={12} lg={8} xl={6}>
+        <Card
+          size="small"
+          title={selectTitle}
+          style={{ width: "100%", marginBottom: "1em" }}
+        >
+          <img
+            src="/maskplaceholder.jpg"
+            style={{ maxWidth: "100%" }}
+            alt="colordepth mip"
+          />
         </Card>
       </Col>
     );
@@ -75,31 +94,55 @@ export default function MaskSelection({ match }) {
     setChannel(e.target.value);
   };
 
+  const handleMaskChange = (maskImageData) => {
+    setMaskedImage(maskImageData);
+  };
+
   const handleSubmit = () => {
+    if (!maskedImage) {
+      message.info('Please select a channel for masking and mask the image');
+      return;
+    }
     // send mask image to server
-    // kick off the search
-    // redirect back to search progress page.
-    history.push('/mysearches');
+    Auth.currentCredentials().then(() => {
+      Storage.put(`${searchMeta.searchDir}/maskedImage.png`, maskedImage, {
+        contentType: maskedImage.type,
+        level: "private",
+        bucket: config.SEARCH_BUCKET
+      })
+        .then(result => {
+          console.log(result);
+          // kick off the search
+          // redirect back to search progress page.
+          history.push("/mysearches");
+        })
+        .catch(e => console.error(e));
+    });
+
   };
 
   let imgSrc;
   if (channel !== null) {
-    imgSrc = `Channel ${channel}`;
+    imgSrc = `/maskplaceholder.jpg?channel=${channel}`;
   }
 
   return (
     <div>
       <Title component="h2">Mask selection</Title>
       <Paragraph>Choose a channel to use in your search</Paragraph>
-      <Radio.Group onChange={handleChannelSelect} value={channel} style={{ display: 'block'}}>
+      <Radio.Group
+        onChange={handleChannelSelect}
+        value={channel}
+        style={{ display: "block" }}
+      >
         <Row gutter={16}>{channelImages}</Row>
       </Radio.Group>
-      <MaskDrawing imgSrc={imgSrc} />
-      <Button type="primary" onClick={handleSubmit}>Submit</Button>
-      <Paragraph>
-        Once mask is uploaded, redirect back to the searches page to show
-        progress.
-      </Paragraph>
+        <Divider orientation="left">Mask the area to search with your mouse</Divider>
+      <MaskDrawing imgSrc={imgSrc} onMaskChange={handleMaskChange} />
+      <Divider />
+      <Button type="primary" onClick={handleSubmit}>
+        Submit
+      </Button>
     </div>
   );
 }
