64420f890de9e2376b9e890b35da2f8c4c426f71
Jody Clements <jody@neomorphic.co.uk>
2020-08-27 16:00:26 -0400

    Fixed External Link tests

--- src/components/__tests__/About.test.jsx
+++ src/components/__tests__/About.test.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import {render} from '@testing-library/react';
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
 import About from "../About";
 
-describe('About: unit tests', () => {
-  it('renders', () => {
-    render(<About/>);
+describe("About: unit tests", () => {
+  it("renders", () => {
+    render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>
+    );
   });
 });
