0624225fc82a4dffe4601b7de22b000538db1dc0
Jody Clements <jody@neomorphic.co.uk>
2020-09-22 15:23:43 -0400

    Adds mirror flip button for mask and match.
    It is now possible to flip either image in the comparison.

--- src/components/ImageComparison.jsx
+++ src/components/ImageComparison.jsx
@@ -1,6 +1,8 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { Row, Col } from "antd";
+import { Row, Col, Button } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRepeat } from "@fortawesome/pro-regular-svg-icons";
 
 import "./ImageComparison.css";
 
@@ -30,8 +32,10 @@ function drawCrosshair(x, y, ctx) {
 }
 
 export default function ImageComparison(props) {
-  const { maskOpen, maskPath, matchPath, matchThumbnail, mirrored } = props;
+  const { maskOpen, maskPath, matchPath, matchThumbnail } = props;
 
+  const [mirrored, setMirrored] = useState(false);
+  const [mirroredMatch, setMirroredMatch] = useState(false);
   const matchRef = useRef();
   const maskRef = useRef();
   const maskImageRef = useRef();
@@ -74,27 +78,69 @@ export default function ImageComparison(props) {
     };
   }, [maskOpen, maskPath, matchPath]);
 
-  const maskStyle = mirrored ? {transform: "scaleX(-1)"} : null;
+  function toggleMirrorMask() {
+    setMirrored(mState => !mState);
+  }
+
+  function toggleMirrorMatch() {
+    setMirroredMatch(mState => !mState);
+  }
+
+  const maskStyle = mirrored ? { transform: "scaleX(-1)" } : null;
+  const matchStyle = mirroredMatch ? { transform: "scaleX(-1)" } : null;
+
+  const maskState = mirrored ? "Restore" : "Flip";
+  const matchState = mirroredMatch ? "Restore" : "Flip";
 
   return (
-    <Row className="imageComparison">
-      {maskOpen && (
-        <Col md={12}>
-          <canvas ref={maskRef} width="500" height="250" />
-          <img src={maskPath} style={maskStyle} ref={maskImageRef} alt="Mask for search" />
+    <>
+      <Row className="imageComparison">
+        {maskOpen && (
+          <Col md={12}>
+            <canvas ref={maskRef} width="500" height="250" />
+            <img
+              src={maskPath}
+              style={maskStyle}
+              ref={maskImageRef}
+              alt="Mask for search"
+            />
+          </Col>
+        )}
+        <Col md={maskOpen ? 12 : 24}>
+          <canvas ref={matchRef} width="500" height="250" />
+          <img
+            src={matchPath || matchThumbnail}
+            style={matchStyle}
+            alt="Search Match"
+          />
         </Col>
-      )}
-      <Col md={maskOpen ? 12 : 24}>
-        <canvas ref={matchRef} width="500" height="250" />
-        <img src={matchPath || matchThumbnail} alt="Search Match" />
-      </Col>
-    </Row>
+      </Row>
+      <Row style={{ marginTop: "0.5em" }}>
+        {maskOpen && (
+          <Col md={12}>
+            <Button
+              icon={<FontAwesomeIcon icon={faRepeat} style={{marginRight: "0.5em"}} />}
+              onClick={() => toggleMirrorMask()}
+            >
+              {maskState} Mask
+            </Button>
+          </Col>
+        )}
+        <Col md={maskOpen ? 12 : 24}>
+          <Button
+            icon={<FontAwesomeIcon icon={faRepeat} style={{marginRight: "0.5em"}} />}
+            onClick={() => toggleMirrorMatch()}
+          >
+           {matchState} Match
+          </Button>
+        </Col>
+      </Row>
+    </>
   );
 }
 
 ImageComparison.propTypes = {
   maskOpen: PropTypes.bool.isRequired,
-  mirrored: PropTypes.bool.isRequired,
   maskPath: PropTypes.string.isRequired,
   matchPath: PropTypes.string.isRequired,
   matchThumbnail: PropTypes.string.isRequired
