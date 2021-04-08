4c369643b0dc56478715b96abaff3b32c6e1801d
Jody Clements <jody@neomorphic.co.uk>
2021-04-08 14:41:44 -0400

    Adds parameters button to Image Alignment.
    Next step is to figure out what this button should do.

--- src/components/CustomSearch/ImageAlignmentStep.jsx
+++ src/components/CustomSearch/ImageAlignmentStep.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { Button } from "antd";
 import { signedLink } from "../../libs/awsLib";
 import StepTitle from "./StepTitle";
 
@@ -7,20 +8,31 @@ export default function ImageAlignmentStep({ state, search }) {
   const [thumbnailUrl, setThumbnailUrl] = useState(null);
 
   useEffect(() => {
-    const uploadUrl = `${search.searchDir}/${search.uploadThumbnail}`;
-    signedLink(uploadUrl).then(result => {
-      setThumbnailUrl(result);
-    });
+    if (search.uploadThumbnail) {
+      const uploadUrl = `${search.searchDir}/${search.uploadThumbnail}`;
+      signedLink(uploadUrl).then(result => {
+        setThumbnailUrl(result);
+      });
+    } else {
+      setThumbnailUrl(null);
+    }
   }, [search.searchDir, search.uploadThumbnail]);
 
   let content = "";
   if (["complete"].includes(state)) {
     content = (
-      <img
-        className="completeThumbnail"
-        src={thumbnailUrl}
-        alt="Alignment Thumbnail"
-      />
+      <>
+        <img
+          className="completeThumbnail"
+          src={thumbnailUrl}
+          alt="Alignment Thumbnail"
+        />
+        <Button
+          style={{ marginTop: "0.5em", width: "150px" }}
+        >
+          parameters
+        </Button>
+      </>
     );
   }
 
