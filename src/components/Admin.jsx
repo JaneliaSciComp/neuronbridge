2eef40c096eb95457b7900fb2f6b2f5527377107
Jody Clements <jody@neomorphic.co.uk>
2021-04-20 10:42:53 -0400

    Removes eslint warnings.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,18 +2,25 @@ import React, { useState, useEffect } from "react";
 import { Auth } from "aws-amplify";
 
 export default function Admin() {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState("");
 
-  useEffect(async () => {
-    const session = await Auth.currentSession();
-    setToken(session.accessToken.jwtToken);
+  useEffect(() => {
+    async function getSession() {
+      const session = await Auth.currentSession();
+      setToken(session.accessToken.jwtToken);
+    }
+    getSession();
   }, []);
 
   return (
     <>
       <h2>Admin Page</h2>
       <p>Bearer token:</p>
-      <textarea style={{ width: "100%", height: "15em" }} value={token} />
+      <textarea
+        readOnly
+        style={{ width: "100%", height: "15em" }}
+        value={token}
+      />
     </>
   );
 }
