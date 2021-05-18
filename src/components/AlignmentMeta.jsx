9e7910bf2debe3dea3e2a70790a698c43349a4e5
Jody Clements <jody@neomorphic.co.uk>
2021-05-18 10:00:13 -0400

    Adds change indicator to alignment parameters.
    This adds an indicator to show which of the individual parameters
    in the alignment have changed.

--- src/components/AlignmentMeta.jsx
+++ src/components/AlignmentMeta.jsx
@@ -1,22 +1,77 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Row, Col } from "antd";
+import { Row, Col, Space, Typography } from "antd";
+
+const { Text } = Typography;
+
+const defaults = {
+  anatomicalRegion: "brain",
+  algorithm: null,
+  referenceChannel: null,
+  voxelX: null,
+  voxelY: null,
+  voxelZ: null
+};
+
+const notDefault = (
+  <span title="Modified" style={{ color: "#ff0000", fontSize: "1.2em" }}>
+    *
+  </span>
+);
+
+// TODO: if voxel size is 0 / null for all dimensions, then show "not selected"
+// TODO: add an asterisk next to items that have been changed from the default
 
 export default function AlignmentMeta({ metadata }) {
   return (
     <Row>
       <Col md={24} lg={1} />
       <Col md={24} lg={7}>
-        <p>
-          <b>Template Matching Algorithm:</b> {metadata.algorithm === "avg" ? "Median Intensity" : "Maximum Intensity" }
-        </p>
-        <p>
-          <b>Reference Channel Index:</b> {metadata.referenceChannel || "Auto-detect"}
-        </p>
-        <p>
-          <b>Voxel Size (microns)</b> {metadata.voxelX} x {metadata.voxelY} x{" "}
-          {metadata.voxelZ}
-        </p>
+        <Space direction="vertical">
+          <Text>
+            <b>
+              Template Matching Algorithm
+              {metadata.algorithm !== defaults.algorithm ? notDefault : ""}:
+            </b>{" "}
+            {metadata.algorithm === "avg"
+              ? "Median Intensity"
+              : "Maximum Intensity"}
+          </Text>
+          <Text>
+            <b>
+              Reference Channel Index
+              {metadata.referenceChannel !== defaults.referenceChannel
+                ? notDefault
+                : ""}
+              :
+            </b>{" "}
+            {metadata.referenceChannel || "Auto-detect"}
+          </Text>
+          <Text>
+            <b>
+              Anatomical Region{" "}
+              {metadata.anatomicalRegion !== defaults.anatomicalRegion
+                ? notDefault
+                : ""}
+              :
+            </b>{" "}
+            {metadata.anatomicalRegion || "brain"}
+          </Text>
+          <Text>
+            <b>
+              Voxel Size (microns)
+              {metadata.voxelX !== defaults.voxelX ? notDefault : ""}:
+            </b>{" "}
+            {metadata.voxelX !== defaults.voxelX ? (
+              <span>
+                {metadata.voxelX} x {metadata.voxelY} x {metadata.voxelZ}
+              </span>
+            ) : (
+              "Not defined"
+            )}
+          </Text>
+          <Text>{notDefault} indicates a change from the default</Text>
+        </Space>
       </Col>
     </Row>
   );
---
+++ src/components/AlignmentMeta.stories.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import AlignmentMeta from "./AlignmentMeta";
+
+import "antd/dist/antd.less";
+
+export default {
+  title: 'AlignmentMeta',
+  component: AlignmentMeta,
+};
+
+const Template = (args) => <AlignmentMeta {...args} />;
+
+export const ModifiedParameters = Template.bind({});
+
+ModifiedParameters.args = {
+  metadata: {
+    anatomicalRegion: "vnc",
+    algorithm: "avg",
+    referenceChannel: 1,
+    voxelX: 5,
+    voxelY: 5,
+    voxelZ: 2
+  }
+};
+
+export const AllDefaults = Template.bind({});
+
+AllDefaults.args = {
+  metadata: {
+    anatomicalRegion: "brain",
+    algorithm: null,
+    referenceChannel: null,
+    voxelX: null,
+    voxelY: null,
+    voxelZ: null
+  }
+};
