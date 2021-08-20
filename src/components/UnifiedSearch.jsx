a4bab7d0b69043bb595699b8ee1aabc3dd93b6b2
Jody Clements <jody@neomorphic.co.uk>
2021-08-20 10:28:42 -0400

    UnifiedSearch updated to use publishedNames lambda
    The lambda function provides more information than the
    publishedNames.txt file and results in less data transfer.

--- src/components/UnifiedSearch.jsx
+++ src/components/UnifiedSearch.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useLocation } from "react-router-dom";
-import { Storage } from "aws-amplify";
+import { Storage, Auth, API } from "aws-amplify";
 import { message } from "antd";
 
 import SearchInput from "./SearchInput";
@@ -20,39 +20,15 @@ export default function UnifiedSearch() {
   const [byBodyResult, setByBodyResults] = useState(null);
   const [lineLoading, setLineLoading] = useState(false);
   const [bodyLoading, setBodyLoading] = useState(false);
-  const [nameslist, setNamesList] = useState(null);
   const [appState] = useContext(AppContext);
-
-  useEffect(() => {
-    if ("precomputedDataRootPath" in appState.paths) {
-      const storageOptions = {
-        customPrefix: {
-          public: ""
-        },
-        level: "public",
-        download: true
-      };
-
-      // TODO: need to have the version directory from paths.json for this to work
-      const publishedNamesUrl = `${appState.paths.precomputedDataRootPath}/publishedNames.txt`;
-      Storage.get(publishedNamesUrl, storageOptions).then(result => {
-        // We can't use result.Body.text() here as it is not supported in safari
-        const fr = new FileReader();
-        fr.onload = evt => {
-          const text = evt.target.result;
-          setNamesList(text.split("\n"));
-        };
-        fr.readAsText(result.Body);
-      });
-    }
-  }, [appState.paths]);
 
   useEffect(() => {
     if ("precomputedDataRootPath" in appState.paths) {
       setByLineResults(null);
       setByBodyResults(null);
 
-      if (!searchTerm || !nameslist) {
+      // don't do anything if we don't have a searchTerm
+      if (!searchTerm) {
         return;
       }
 
@@ -86,104 +62,149 @@ export default function UnifiedSearch() {
         download: true
       };
 
-      // get the list of names that match the query
-      let matchedNames = [];
+      Auth.currentCredentials().then(() => {
+        API.get("SearchAPI", "/published_names", {
+          queryStringParameters: { q: searchTerm }
+        }).then(items => {
+          const lineCombined = { results: [] };
+          const bodyCombined = { results: [] };
 
-      if (nameslist) {
-        const match = new RegExp(`^${searchTerm.replace(/\*/g, ".*")}$`, "i");
-        matchedNames = nameslist.filter(item => {
-          return item.match(match);
-        });
-      }
+          const allItems =
+            items.names.sort((a,b) => {
+              if (a.key === b.key) {
+                return 0;
+              }
+              if (a.key < b.key) {
+                return -1;
+              }
+              return 1;
+            }).map(match => {
+              if (match.keyType === "publishingName") {
+                const byLineUrl = `${appState.paths.precomputedDataRootPath}/metadata/by_line/${match.key}.json`;
+                return Storage.get(byLineUrl, storageOptions)
+                  .then(metaData => {
+                    // We can't use metaData.Body.text() here as it is not supported in safari
+                    const fr = new FileReader();
+                    fr.onload = evt => {
+                      const text = evt.target.result;
+                      const newResults = JSON.parse(text);
+                      // convert stored relative urls into the full path urls.
+                      const urlFixedResults = newResults.results.map(result => {
+                        const fullImageUrl = `${appState.paths.imageryBaseURL}/${result.imageURL}`;
+                        const fullThumbUrl = `${appState.paths.thumbnailsBaseURLs}/${result.thumbnailURL}`;
+                        return {
+                          ...result,
+                          imageURL: fullImageUrl,
+                          thumbnailURL: fullThumbUrl
+                        };
+                      });
+                      lineCombined.results.push(...urlFixedResults);
+                      setByLineResults({ ...lineCombined });
+                    };
+                    fr.readAsText(metaData.Body);
+                  })
+                  .catch(error => {
+                    if (error === "No credentials") {
+                      // Log me out and prompt me to login again.
+                    }
+                    lineCombined.error = error;
+                    setByLineResults(lineCombined);
+                  });
+              }
+              if (match.keyType === "bodyID") {
+                const byBodyUrl = `${appState.paths.precomputedDataRootPath}/metadata/by_body/${match.key}.json`;
+                return Storage.get(byBodyUrl, storageOptions)
+                  .then(metaData => {
+                    // We can't use metaData.Body.text() here as it is not supported in safari
+                    const fr = new FileReader();
+                    fr.onload = evt => {
+                      const text = evt.target.result;
+                      const newResults = JSON.parse(text);
+                      // convert stored relative urls into the full path urls.
+                      const urlFixedResults = newResults.results.map(result => {
+                        const fullImageUrl = `${appState.paths.imageryBaseURL}/${result.imageURL}`;
+                        const fullThumbUrl = `${appState.paths.thumbnailsBaseURLs}/${result.thumbnailURL}`;
+                        return {
+                          ...result,
+                          imageURL: fullImageUrl,
+                          thumbnailURL: fullThumbUrl
+                        };
+                      });
+                      bodyCombined.results.push(...urlFixedResults);
+                      setByBodyResults({ ...bodyCombined });
+                    };
+                    fr.readAsText(metaData.Body);
+                  })
+                  .catch(error => {
+                    if (error === "No credentials") {
+                      // Log me out and prompt me to login again.
+                    }
+                    bodyCombined.error = error;
+                    setByBodyResults(bodyCombined);
+                  });
+              }
+              if (match.keyType === "neuronInstance" || match.keyType === "neuronType") {
+                return match.bodyIDs.map(bodyID => {
+                  const byBodyUrl = `${appState.paths.precomputedDataRootPath}/metadata/by_body/${bodyID}.json`;
+                  return Storage.get(byBodyUrl, storageOptions)
+                    .then(metaData => {
+                      // We can't use metaData.Body.text() here as it is not supported in safari
+                      const fr = new FileReader();
+                      fr.onload = evt => {
+                        const text = evt.target.result;
+                        const newResults = JSON.parse(text);
+                        // convert stored relative urls into the full path urls.
+                        const urlFixedResults = newResults.results.map(result => {
+                          const fullImageUrl = `${appState.paths.imageryBaseURL}/${result.imageURL}`;
+                          const fullThumbUrl = `${appState.paths.thumbnailsBaseURLs}/${result.thumbnailURL}`;
+                          return {
+                            ...result,
+                            imageURL: fullImageUrl,
+                            thumbnailURL: fullThumbUrl
+                          };
+                        });
+                        bodyCombined.results.push(...urlFixedResults);
+                        setByBodyResults({ ...bodyCombined });
+                      };
+                      fr.readAsText(metaData.Body);
+                    })
+                    .catch(error => {
+                      if (error === "No credentials") {
+                        // Log me out and prompt me to login again.
+                      }
+                      bodyCombined.error = error;
+                      setByBodyResults(bodyCombined);
+                    });
+                });
+              }
+              console.log(match.keyType, match);
+              return Promise.resolve();
+            });
 
-      // set a limit on the number of items that can be searched,
-      // so we don't break the site. Need to think this over.
-      matchedNames = matchedNames
-        .sort((a, b) =>
-          a.localeCompare(b, undefined, { numeric: true, sensitivity: "base" })
-        )
-        .slice(0, 100);
+          const allPromisses = Promise.all(allItems.flat());
 
-      const lineNames = matchedNames.filter(name => name.match(/[a-z]/i));
-      const lineCombined = { results: [] };
-      lineNames.forEach(name => {
-        const byLineUrl = `${appState.paths.precomputedDataRootPath}/metadata/by_line/${name}.json`;
-        Storage.get(byLineUrl, storageOptions)
-          .then(metaData => {
-            // We can't use metaData.Body.text() here as it is not supported in safari
-            const fr = new FileReader();
-            fr.onload = evt => {
-              const text = evt.target.result;
-              const newResults = JSON.parse(text);
-              // convert stored relative urls into the full path urls.
-              const urlFixedResults = newResults.results.map(result => {
-                const fullImageUrl = `${appState.paths.imageryBaseURL}/${result.imageURL}`;
-                const fullThumbUrl = `${appState.paths.thumbnailsBaseURLs}/${result.thumbnailURL}`;
-                return {...result, imageURL: fullImageUrl, thumbnailURL: fullThumbUrl};
-              });
-              lineCombined.results.push(...urlFixedResults);
-              setByLineResults({ ...lineCombined });
-              setLineLoading(false);
-            };
-            fr.readAsText(metaData.Body);
-          })
-          .catch(error => {
-            if (error === "No credentials") {
-              // Log me out and prompt me to login again.
+          // once all the items have loaded, we can clean up.
+          allPromisses.then(() => {
+            if (lineCombined.results.length === 0) {
+              setByLineResults({ results: [] });
             }
-            setByLineResults({ error, results: [] });
+
+            if (bodyCombined.results.length === 0) {
+              setByBodyResults({ results: [] });
+            }
             setLineLoading(false);
-          });
-      });
-
-      if (lineNames.length === 0) {
-        setByLineResults({ results: [] });
-        setLineLoading(false);
-      }
-
-      const bodyIds = matchedNames.filter(name => !name.match(/[a-z]/i));
-      const bodyCombined = { results: [] };
-      bodyIds.forEach(name => {
-        const byBodyUrl = `${appState.paths.precomputedDataRootPath}/metadata/by_body/${name}.json`;
-        Storage.get(byBodyUrl, storageOptions)
-          .then(metaData => {
-            // We can't use metaData.Body.text() here as it is not supported in safari
-            const fr = new FileReader();
-            fr.onload = evt => {
-              const text = evt.target.result;
-              const newResults = JSON.parse(text);
-              // convert stored relative urls into the full path urls.
-              const urlFixedResults = newResults.results.map(result => {
-                const fullImageUrl = `${appState.paths.imageryBaseURL}/${result.imageURL}`;
-                const fullThumbUrl = `${appState.paths.thumbnailsBaseURLs}/${result.thumbnailURL}`;
-                return {...result, imageURL: fullImageUrl, thumbnailURL: fullThumbUrl};
-              });
-              bodyCombined.results.push(...urlFixedResults);
-              setByBodyResults({ ...bodyCombined });
-              setBodyLoading(false);
-            };
-            fr.readAsText(metaData.Body);
-          })
-          .catch(error => {
-            if (error === "No credentials") {
-              // Log me out and prompt me to login again.
-            }
-            setByBodyResults({ error, results: [] });
             setBodyLoading(false);
-          });
-      });
 
-      if (bodyIds.length === 0) {
-        setByBodyResults({ results: [] });
-        setBodyLoading(false);
-      }
+          });
+        });
+      });
     }
-  }, [searchTerm, nameslist, appState.paths]);
+  }, [searchTerm, appState.paths]);
 
   return (
     <div>
       <SearchInput searchTerm={searchTerm} />
-      {!searchTerm && !byLineResult && !byBodyResult && <NoSearch />}
+      {!searchTerm && <NoSearch />}
       {(lineLoading || bodyLoading) && <p>loading...</p>}
       {byLineResult && byBodyResult && (
         <UnifiedSearchResults
