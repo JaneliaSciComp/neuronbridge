424daefa0cb915f54a3b70cabcd86f90fb7e767f
Jody Clements <jody@neomorphic.co.uk>
2021-09-09 14:36:09 -0400

    Sets maintenance warning to info instead of error.

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -4,8 +4,9 @@ import { Alert } from "antd";
 export default function Maintenance() {
   return (
     <Alert
-      style={{margin: "1em"}}
-      type="error"
+      style={{ margin: "2em", textAlign: "center" }}
+      type="info"
+      showIcon
       message="Our search service is down for maintenance and will return shortly"
     />
   );
