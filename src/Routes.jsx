df0210b7b0abf528bd8dc97636327fb1b3471858
Jody Clements <jody@neomorphic.co.uk>
2021-09-16 14:34:11 -0400

    Names lazy loaded components via webpack.
    This makes it easier to see which component has loaded when looking in
    the network tab of the developer console.

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -8,22 +8,23 @@ import SearchErrorBoundary from "./components/SearchErrorBoundary";
 import Landing from "./components/Landing";
 import config from "./config";
 
-const ReleaseNotes = React.lazy(() => import("./components/ReleaseNotes"));
-const About = React.lazy(() => import("./components/About"));
-const Admin = React.lazy(() => import("./components/Admin"));
-const Login = React.lazy(() => import("./components/Login"));
-const HelpPage = React.lazy(() => import("./components/HelpPage"));
-const Signup = React.lazy(() => import("./components/Signup"));
-const Search = React.lazy(() => import("./components/Search"));
-const Results = React.lazy(() => import("./components/Results"));
-const Maintenance = React.lazy(() => import("./components/Maintenance"));
-const MaskSelection = React.lazy(() => import("./components/MaskSelection"));
+const ReleaseNotes = React.lazy(() => import(/* webpackChunkName: 'rel-notes' */"./components/ReleaseNotes"));
+const About = React.lazy(() => import(/* webpackChunkName: 'about' */ "./components/About"));
+const Admin = React.lazy(() => import(/* webpackChunkName: 'admin' */ "./components/Admin"));
+const Login = React.lazy(() => import(/* webpackChunkName: 'login' */ "./components/Login"));
+const HelpPage = React.lazy(() => import(/* webpackChunkName: 'help' */ "./components/HelpPage"));
+const Signup = React.lazy(() => import(/* webpackChunkName: 'signup' */ "./components/Signup"));
+const Search = React.lazy(() => import(/* webpackChunkName: 'search' */ "./components/Search"));
+const Results = React.lazy(() => import(/* webpackChunkName: 'results' */ "./components/Results"));
+const Maintenance = React.lazy(() => import(/* webpackChunkName: 'maint' */ "./components/Maintenance"));
+const MaskSelection = React.lazy(() => import(/* webpackChunkName: 'mask' */ "./components/MaskSelection"));
 const CustomSearchList = React.lazy(() =>
+  /* webpackChunkName: 'custom-search-list' */
   import("./components/CustomSearchList")
 );
-const NotFound = React.lazy(() => import("./components/NotFound"));
-const UsageTerms = React.lazy(() => import("./components/UsageTerms"));
-const ResetPassword = React.lazy(() => import("./components/ResetPassword"));
+const NotFound = React.lazy(() => import(/* webpackChunkName: 'notfound' */ "./components/NotFound"));
+const UsageTerms = React.lazy(() => import(/* webpackChunkName: 'usage' */ "./components/UsageTerms"));
+const ResetPassword = React.lazy(() => import(/* webpackChunkName: 'reset-pass' */ "./components/ResetPassword"));
 
 export default function Routes({ appProps }) {
   return (
