e8ee64bbb4475380a0500e0cd7bf335f33aa1b02
Jody Clements <jody@neomorphic.co.uk>
2021-09-23 09:57:58 -0400

    Adds correct columns for csv export of PPPM results.

--- src/components/ResultsExport.jsx
+++ src/components/ResultsExport.jsx
@@ -5,13 +5,23 @@ import { CSVLink } from "react-csv";
 export default function ResultsExport({ results, searchType }) {
   const name = searchType === "lines" ? "Body Id" : "Line Name";
 
-  const headers = [
+  let headers = [
     { label: "Number", key: "position" },
     { label: name, key: "publishedName" },
     { label: "Score", key: "normalizedScore" },
     { label: "Matched Pixels", key: "matchingPixels" },
     { label: "Library", key: "libraryName" }
   ];
+
+  if (searchType === "ppp") {
+    headers = [
+      { label: "Number", key: "position" },
+      { label: name, key: "publishedName" },
+      { label: "Rank", key: "pppRank" },
+      { label: "Score", key: "pppScore" },
+      { label: "Library", key: "libraryName" }
+    ];
+  }
 
   if (searchType !== "lines") {
     headers.push({ label: "Slide Code", key: "slideCode" });
