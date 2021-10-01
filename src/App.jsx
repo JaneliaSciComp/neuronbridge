4f4d0363128a0e74b2d1eacf8316c66ea56d9f7d
Jody Clements <jody@neomorphic.co.uk>
2021-10-01 09:26:45 -0400

    Moves App.css import after antd.less import.
    This makes sure that our css overrides the values in the antd
    stylesheet.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { faEnvelope } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Routes from "./Routes";
 import LoggedInAs from "./components/LoggedInAs";
-import "./App.css";
 import config from "./config";
 import janeliaLogo from "./janelia_logo.png";
 import flyemLogo from "./flyemLogo.png";
@@ -17,6 +16,7 @@ import HelpDrawer from "./components/Help/HelpDrawer";
 import HelpContents from "./components/Help/HelpContents";
 import SiteSurvey from "./components/SiteSurvey";
 import "antd/dist/antd.less";
+import "./App.css";
 
 const { Header, Content, Footer } = Layout;
 
