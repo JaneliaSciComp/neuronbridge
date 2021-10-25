e55ed198adce5707fcd5d742a591da35ac60f8c5
Jody Clements <jody@neomorphic.co.uk>
2021-10-25 14:52:57 -0400

    Adds Start & End date filtering to announcements.

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -17,39 +17,51 @@ export default function Announcements({ source }) {
 
   const formatted = announcements
     .filter(ann => {
-      return ann.message
-      // TODO: only messages that start before current time and end after current
+      // only messages that start before current time and end after current
       // time should be shown.
+      const now = Date.now();
+      if (Date.parse(ann.start) <= now && now <= Date.parse(ann.end)) {
+        return true;
+      }
+      return false;
     })
-    .sort((a,b) => {
-    // sort messages so newest are at the top.
-    return new Date(b.start) - new Date(a.start)
-  }).map(ann => {
-    const message = ann.stamp
-      ? `${new Date(ann.start).toLocaleString()} - ${ann.message}`
-      : ann.message;
+    .sort((a, b) => {
+      // sort messages so newest are at the top.
+      return new Date(b.start) - new Date(a.start);
+    })
+    .map(ann => {
+      const message = ann.stamp
+        ? `${new Date(ann.start).toLocaleString()} - ${ann.message}`
+        : ann.message;
 
-    const action = ann.action ? (
-      <Link to={ann.action.link}>
-        <Button size="small" type="ghost">
-          {ann.action.text}
-        </Button>
-      </Link>
-    ) : null;
+      const action = ann.action ? (
+        <Link to={ann.action.link}>
+          <Button size="small" type="ghost">
+            {ann.action.text}
+          </Button>
+        </Link>
+      ) : null;
 
-    return (
-      <Alert
-        style={{ marginBottom: "1em" }}
-        key={ann.start}
-        message={message}
-        type={ann.type || "info"}
-        closable={ann.closable}
-        action={action}
-      />
-    );
-  });
+      return (
+        <Alert
+          style={{ marginBottom: "1em" }}
+          key={ann.start}
+          message={message}
+          type={ann.type || "info"}
+          closable={ann.closable}
+          action={action}
+        />
+      );
+    });
 
-  return <>{formatted}</>;
+  return (
+    <>
+      {formatted}
+      {formatted.length > 0 ? (
+        <Link to="/announcements" style={{float: "left"}}>See all announcements</Link>
+      ) : null}
+    </>
+  );
 }
 
 Announcements.propTypes = {
