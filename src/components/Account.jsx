be78cc7ab5d2f8935976b0293478b25efb2862ff
Jody Clements <jody@neomorphic.co.uk>
2021-11-09 11:21:09 -0500

    Updates account page with better copy.
    The email preferences section looks better with some text describing
    what it is about.

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Auth } from "aws-amplify";
-import { Checkbox, Typography, message } from "antd";
+import { Card, Checkbox, Typography, message } from "antd";
 
 const { Title } = Typography;
 
 export default function Account() {
-  const [survey, setSurvey] = useState(false);
+  // const [survey, setSurvey] = useState(false);
   const [newsLetter, setNewsLetter] = useState(false);
   const [loading, setLoading] = useState(true);
 
@@ -14,7 +14,7 @@ export default function Account() {
     // survey and newsletter preferences to the state.
     async function getUserInfo() {
       const userInfo = await Auth.currentUserInfo();
-      setSurvey(userInfo.attributes["custom:survey"] === "true");
+      // setSurvey(userInfo.attributes["custom:survey"] === "true");
       setNewsLetter(userInfo.attributes["custom:newsletter"] === "true");
       setLoading(false);
     }
@@ -27,21 +27,25 @@ export default function Account() {
     await Auth.updateUserAttributes(user, {
       "custom:newsletter": event.target.checked.toString()
     }).catch(() => {
-      message.error("Unable to change newsletter preference, please try again later.");
-      setNewsLetter(!event.target.checked)
+      message.error(
+        "Unable to change newsletter preference, please try again later."
+      );
+      setNewsLetter(!event.target.checked);
     });
   };
 
-  const handleSurveyChange = async event => {
+  /* const handleSurveyChange = async event => {
     setSurvey(event.target.checked);
     const user = await Auth.currentAuthenticatedUser();
     await Auth.updateUserAttributes(user, {
       "custom:survey": event.target.checked.toString()
     }).catch(() => {
-      message.error("Unable to change survey preference, please try again later.");
+      message.error(
+        "Unable to change survey preference, please try again later."
+      );
       setSurvey(!event.target.checked);
     });
-  };
+  }; */
 
   return (
     <>
@@ -50,14 +54,18 @@ export default function Account() {
         <p>Loading...</p>
       ) : (
         <>
-          <Checkbox checked={newsLetter} onChange={handleNewsLetterChange}>
-            {" "}
-            Email newsletter
-          </Checkbox>
-          <Checkbox checked={survey} onChange={handleSurveyChange}>
-            {" "}
-            Feedback surveys
-          </Checkbox>
+          <Card
+            title="Updates and New Releases"
+            style={{ marginBottom: "1em" }}
+          >
+            <p>
+              Periodically, we will send out an email newsletter describing
+              updates and new releases relating to NeuronBridge. By opting in
+              you grant us permission to send emails to the email address you
+              provided when logging in to this site.
+            </p>
+            <Checkbox checked={newsLetter} onChange={handleNewsLetterChange} > Yes, please send me updates. </Checkbox>
+          </Card>
         </>
       )}
     </>
