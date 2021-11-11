393705cd1140da13b6772edaa30d78b2693273e7
Jody Clements <jody@neomorphic.co.uk>
2021-11-11 16:19:42 -0500

    Adds dependencies to placeholder useEffect hook.

--- src/components/ImagePlaceholder.jsx
+++ src/components/ImagePlaceholder.jsx
@@ -56,7 +56,7 @@ export default function ImagePlaceholder({
         img.src = signed;
       });
     }
-  }, [src]);
+  }, [src, imageLoaded]);
 
   function handleLoaded(imgSrc) {
     setLoaded(imgSrc);
