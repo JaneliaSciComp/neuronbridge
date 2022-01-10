a90a322f4e90f4021bd698e2f47954647e72e28c
Jody Clements <jody@neomorphic.co.uk>
2022-01-10 16:47:47 -0500

    Fixes two tests that needed AppContext

--- src/components/__tests__/CustomInputSummary.test.jsx
+++ src/components/__tests__/CustomInputSummary.test.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import CustomInputSummary from "../CustomInputSummary";
+import { AppProvider } from "../../containers/AppContext";
 
 function PlaceHolder() {
-  return (
-    <p>PlaceHolder</p>
-  );
+  return <p>PlaceHolder</p>;
 }
 
 describe("CustomInputSummary: unit tests", () => {
   it("renders", async () => {
     render(
-      <CustomInputSummary searchMeta={{
-        upload: "test-file",
-        updatedOn: "2021-05-04T13:18:58.915Z",
-        createdOn: "2021-05-04T13:16:52.663Z",
-        thumbnailURL: "http://example.com",
-        imageURL: "http://example.com",
-        alignFinished: true
-      }}>
-        <PlaceHolder/>
-        <PlaceHolder/>
-      </CustomInputSummary>
-
+      <AppProvider>
+        <CustomInputSummary
+          searchMeta={{
+            upload: "test-file",
+            updatedOn: "2021-05-04T13:18:58.915Z",
+            createdOn: "2021-05-04T13:16:52.663Z",
+            thumbnailURL: "http://example.com",
+            imageURL: "http://example.com",
+            alignFinished: true
+          }}
+        >
+          <PlaceHolder />
+          <PlaceHolder />
+        </CustomInputSummary>
+      </AppProvider>
     );
     expect(await screen.getByText(/Input Mask/i));
   });
--- src/components/__tests__/HelpPage.test.jsx
+++ src/components/__tests__/HelpPage.test.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import HelpPage from "../HelpPage";
+import { AppProvider } from "../../containers/AppContext";
 
 describe("HelpPage: unit tests", () => {
   it("renders", () => {
     const { getByText } = render(
-      <MemoryRouter>
-        <HelpPage />
-      </MemoryRouter>
+      <AppProvider>
+        <MemoryRouter>
+          <HelpPage />
+        </MemoryRouter>
+      </AppProvider>
     );
     expect(getByText(/Obtaining the raw data/i));
   });
