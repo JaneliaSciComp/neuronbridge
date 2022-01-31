a5bff482851a05a41c7aead229399b29330ca5fc
Jody Clements <jody@neomorphic.co.uk>
2022-01-31 15:33:03 -0500

    Debug panel shows where data was loaded from.

--- src/components/DebugPanel.jsx
+++ src/components/DebugPanel.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { Row, Col } from "antd";
+import { Row, Col, Card } from "antd";
 import { Auth, API } from "aws-amplify";
 
 export default function DebugPanel({ appState, config }) {
@@ -9,13 +9,13 @@ export default function DebugPanel({ appState, config }) {
 
   useEffect(() => {
     Auth.currentCredentials().then(() => {
-        API.get("SearchAPI", "/published_names", {
-          queryStringParameters: { version: true }
-        }).then(data => {
-          setPublishedNames(data.version);
-        });
+      API.get("SearchAPI", "/published_names", {
+        queryStringParameters: { version: true }
+      }).then(data => {
+        setPublishedNames(data.version);
+      });
     });
-  },[publishedNames]);
+  }, [publishedNames]);
 
   const searchEndpoints = config.api.endpoints.map(endpoint => (
     <p key={endpoint.name}>
@@ -23,48 +23,69 @@ export default function DebugPanel({ appState, config }) {
     </p>
   ));
 
+  const searchEndpointURL = config.api.endpoints.map(
+    endpoint => endpoint.endpoint
+  );
+
   return (
     <>
-    <Row gutter={8}>
-      <Col span={12} style={{ textAlign: "left" }}>
-        <p>
-          <b>Data Bucket:</b> {config.s3.BUCKET}
-        </p>
-        <p>
-          <b>Search Bucket:</b> {config.SEARCH_BUCKET}
-        </p>
-        <p>
-          <b>PPPM Bucket:</b> {config.PPPM_BUCKET}
-        </p>
-        {searchEndpoints}
-        <p>
-          <b>GraphQL:</b> {config.appsync.graphqlEndpoint}
-        </p>
-        <p>
-          <b>App Level:</b> {config.APP_LEVEL}
-        </p>
-      </Col>
-      <Col span={12} style={{ textAlign: "left" }}>
-        <p>
-          <b>Data version:</b> {appState.dataVersion}
-        </p>
-        <p>
-          <b>Imagery Base URL:</b> {dataConfig.imageryBaseURL}
-        </p>
-        <p>
-          <b>Thumbnails Base URL:</b> {dataConfig.thumbnailsBaseURLs}
-        </p>
-        <p>
-          <b>PPP Imagery Base URL:</b> {dataConfig.pppImageryBaseURL}
-        </p>
-        <p>
-          <b>NODE_ENV:</b> {process.env.NODE_ENV}
-        </p>
-        <p>
-          <b>Published Names table:</b> {publishedNames}
-        </p>
-      </Col>
-    </Row>
+      <Row gutter={[16, 16]} style={{ marginBottom: "1rem" }}>
+        <Col span={12} style={{ textAlign: "left" }}>
+          <Card size="small" title="Set during website code build">
+            <p>
+              <b>Data Bucket:</b> {config.s3.BUCKET}
+            </p>
+            <p>
+              <b>Search Bucket:</b> {config.SEARCH_BUCKET}
+            </p>
+            <p>
+              <b>PPPM Bucket:</b> {config.PPPM_BUCKET}
+            </p>
+            {searchEndpoints}
+            <p>
+              <b>GraphQL:</b> {config.appsync.graphqlEndpoint}
+            </p>
+            <p>
+              <b>App Level:</b> {config.APP_LEVEL}
+            </p>
+            <p>
+              <b>NODE_ENV:</b> {process.env.NODE_ENV}
+            </p>
+          </Card>
+        </Col>
+        <Col span={12} style={{ textAlign: "left" }}>
+          <Card
+            size="small"
+            title={`s3://${config.s3.BUCKET}/current.txt`}
+            style={{ marginBottom: "1rem" }}
+          >
+            <p>
+              <b>Data version:</b> {appState.dataVersion}
+            </p>
+          </Card>
+          <Card
+            size="small"
+            title={`s3://${config.s3.BUCKET}/${appState.dataVersion}/config.json`}
+            style={{ marginBottom: "1rem" }}
+          >
+            <p>
+              <b>Imagery Base URL:</b> {dataConfig.imageryBaseURL}
+            </p>
+            <p>
+              <b>Thumbnails Base URL:</b> {dataConfig.thumbnailsBaseURLs}
+            </p>
+            <p>
+              <b>PPP Imagery Base URL:</b> {dataConfig.pppImageryBaseURL}
+            </p>
+          </Card>
+
+          <Card size="small" title={`${searchEndpointURL[0]}/published_names`}>
+            <p>
+              <b>Published Names table:</b> {publishedNames}
+            </p>
+          </Card>
+        </Col>
+      </Row>
     </>
   );
 }
