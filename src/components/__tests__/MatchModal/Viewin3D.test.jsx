5a870abdda7a7120cf8e7a7ee44f4d5a6fa02bfb
Jody Clements <jody@neomorphic.co.uk>
2022-02-23 09:47:49 -0500

    Fixes ViewIn3D tests

--- src/components/__tests__/MatchModal/Viewin3D.test.jsx
+++ src/components/__tests__/MatchModal/Viewin3D.test.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import ViewIn3D from "../../MatchModal/ViewIn3D";
+import { AppProvider } from "../../../containers/AppContext";
 
 describe("ViewIn3D: unit tests", () => {
   it("renders", () => {
     render(
-      <MemoryRouter>
-        <ViewIn3D
-          selectedMatch={{}}
-          mask={{}}
-        />
-      </MemoryRouter>
+      <AppProvider>
+        <MemoryRouter>
+          <ViewIn3D selectedMatch={{}} mask={{}} isLM={false} />
+        </MemoryRouter>
+      </AppProvider>
     );
   });
 });
