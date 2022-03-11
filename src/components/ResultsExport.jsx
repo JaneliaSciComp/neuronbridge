339edb1b9ab377624e8010ef7e5cd3f85dec4c0c
Jody Clements <jody@neomorphic.co.uk>
2022-03-11 15:54:42 -0500

    Adds more metadata to matches export.

--- src/components/ResultsExport.jsx
+++ src/components/ResultsExport.jsx
@@ -8,30 +8,33 @@ export default function ResultsExport({ results, searchType }) {
   // TODO: create a list of headers that we want to have in the export.
   // loop over the keys in the first result item and if that key exists
   // in the list, then add it to the headers we are going to use.
-
-  let headers = [
+  const headersList = [
     { label: "Number", key: "position" },
     { label: name, key: "publishedName" },
+    { label: "Rank", key: "pppRank" },
     { label: "Score", key: "normalizedScore" },
+    { label: "Score", key: "pppScore" },
     { label: "Matched Pixels", key: "matchingPixels" },
     { label: "Library", key: "libraryName" },
     { label: "Channel", key: "channel" },
-    { label: "Magnification", key: "objective" }
+    { label: "Magnification", key: "objective" },
+    { label: "Slide Code", key: "slideCode" },
+    { label: "Gender", key: "gender" },
+    { label: "Alignment Space", key: "alignmentSpace" },
+    { label: "Mounting Protocol", key: "mountingProtocol" },
+    { label: "Anatomical Area", key: "anatomicalArea" },
+    { label: "Neuron Type", key: "neuronType" },
+    { label: "Neuron Instance", key: "neuronInstance" }
   ];
 
-  if (searchType === "ppp") {
-    headers = [
-      { label: "Number", key: "position" },
-      { label: name, key: "publishedName" },
-      { label: "Rank", key: "pppRank" },
-      { label: "Score", key: "pppScore" },
-      { label: "Library", key: "libraryName" }
-    ];
-  }
+  const dataKeys = Object.keys(results[0]);
 
-  if (searchType !== "lines") {
-    headers.push({ label: "Slide Code", key: "slideCode" });
-  }
+  const headers = headersList.map(header => {
+    if (dataKeys.includes(header.key)) {
+      return header;
+    }
+    return undefined;
+  }).filter(header => header);
 
   return (
       <CSVLink
