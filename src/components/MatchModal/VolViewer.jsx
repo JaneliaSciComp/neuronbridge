9eaaaa60c3b9d9d6cb2a3c3a50dcbbf9cf11940b
Jody Clements <jody@neomorphic.co.uk>
2022-05-03 10:26:45 -0400

    Adds H5j3dViewerWithBasicUI for testing

---
+++ src/components/MatchModal/VolViewer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import { Vol3dViewer } from '@janelia/web-vol-viewer';
+import { H5j3dViewerWithBasicUI } from "@janelia/web-vol-viewer";
+
+export default function VolViewer() {
+  return (
+    <>
+      <H5j3dViewerWithBasicUI />
+    </>
+  );
+}
