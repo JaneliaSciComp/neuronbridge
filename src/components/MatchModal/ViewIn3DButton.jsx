2f27f2b6c581503d449a98e60c5c376eb89c4e54
Jody Clements <jody@neomorphic.co.uk>
2022-08-23 09:48:26 -0400

    Adds 3D cube to view in 3D buttons.

--- src/components/MatchModal/ViewIn3DButton.jsx
+++ src/components/MatchModal/ViewIn3DButton.jsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import { Button } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCube } from "@fortawesome/pro-regular-svg-icons";
 import config from "../../config";
 import { AppContext } from "../../containers/AppContext";
 import { signedPublicLink } from "../../libs/awsLib";
@@ -16,26 +18,23 @@ function getSWCLink(baseURL, construct) {
 }
 
 export default function ViewIn3DButton({ isLM, match, mask, style }) {
-  const [ signedSwc, setSignedSwc ] = React.useState(null);
+  const [signedSwc, setSignedSwc] = React.useState(null);
   const { appState } = React.useContext(AppContext);
   const ref = window.location;
   const h5j = isLM ? getH5JLink(match) : getH5JLink(mask);
   const channel = isLM
     ? parseInt(match.channel, 10)
     : parseInt(mask.channel, 10);
-  const { mirrored }  = match
+  const { mirrored } = match;
 
   React.useEffect(() => {
     const swc = isLM
       ? getSWCLink(appState.dataConfig.swcBaseURL, mask)
       : getSWCLink(appState.dataConfig.swcBaseURL, match);
-    signedPublicLink(swc.props.href).then(signed =>  {
+    signedPublicLink(swc.props.href).then((signed) => {
       setSignedSwc(signed);
     });
-  },[appState.dataConfig.swcBaseURL, isLM, mask, match]);
-
-
-
+  }, [appState.dataConfig.swcBaseURL, isLM, mask, match]);
 
   // must encode the signedSWC, so that it makes it to the volume viewer in the
   // correct state for loading the swc
@@ -60,6 +59,7 @@ export default function ViewIn3DButton({ isLM, match, mask, style }) {
       ghost
       style={style}
       onClick={handleClick}
+      icon={<FontAwesomeIcon icon={faCube} size="lg" transform="left-6" />}
     >
       View in 3D
     </Button>
@@ -74,5 +74,5 @@ ViewIn3DButton.propTypes = {
 };
 
 ViewIn3DButton.defaultProps = {
-  style: {}
+  style: {},
 };
