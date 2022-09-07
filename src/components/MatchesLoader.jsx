81f544b514f8dad3adeff561d655e856b21504ad
Jody Clements <jody@neomorphic.co.uk>
2022-09-07 09:27:58 -0400

    Adds tabs for CDM & PPPM results to matches page.
    This adds tabs for both CDM and PPPM results to the matches page, but it
    does not check to see if both searches have been run for this line/body
    id.

--- src/components/MatchesLoader.jsx
+++ src/components/MatchesLoader.jsx
@@ -1,23 +1,14 @@
 import React, { useEffect, useState, useContext } from "react";
 import PropTypes from "prop-types";
 import { Auth, Storage } from "aws-amplify";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { Spin, Divider, message } from "antd";
 import SearchSummary from "./SearchSummary";
 import Matches from "./Matches";
 import { AppContext } from "../containers/AppContext";
 import { MatchesProvider } from "../containers/MatchesContext";
 
-function formatFullImageUrl(baseUrl, result) {
-  return `${baseUrl}${result.image.files.ColorDepthMip || result.files.ColorDepthMip}`;
-}
-
-function formatFullImageThumbnailUrl(baseUrl, result, searchType) {
-  if (searchType === "ppp") {
-    return `${baseUrl}${result.files.ColorDepthMip.replace(/\.png$/, '.jpg')}`;
-  }
-  return `${baseUrl}${result.image.files.ColorDepthMipThumbnail}`;
-}
+import "./MatchesLoader.css";
 
 export default function MatchesLoader({ searchResult, searchType }) {
   const [isLoading, setLoading] = useState(false);
@@ -28,10 +19,10 @@ export default function MatchesLoader({ searchResult, searchType }) {
   useEffect(() => {
     const storageOptions = {
       customPrefix: {
-        public: ""
+        public: "",
       },
       level: "public",
-      download: true
+      download: true,
     };
 
     function getMatches() {
@@ -41,51 +32,61 @@ export default function MatchesLoader({ searchResult, searchType }) {
         metadataPath = `${appState.dataVersion}/metadata/pppresults/${searchTerm}.json`;
       }
 
-      Auth.currentCredentials().then(() => {
-        Storage.get(metadataPath, storageOptions).then(response => {
-          const fr = new FileReader();
-          fr.onload = evt => {
-            const json = JSON.parse(evt.target.result);
-            const fixedResults = json.results.map(result => {
-              const fullImageUrl = formatFullImageUrl(appState.dataConfig.prefixes.ColorDepthMip, result);
-              const fullThumbUrl = formatFullImageThumbnailUrl(appState.dataConfig.prefixes.ColorDepthMipThumbnail, result, searchType);
-              const fixedResult = {
-                ...result,
-                imageURL: fullImageUrl,
-                thumbnailURL: fullThumbUrl
-              };
-              // The ppp results json starts the rank at 0, but the pdfs start
-              // the rank at 1, so we need to add 1 to the pppRank in the JSON
-              // to get the same rank in the UI as is displayed in the pdf.
-              if (searchType === "ppp") {
-                fixedResult.pppRank = result.pppRank + 1;
-              }
-              return fixedResult;
-            });
+      Auth.currentCredentials()
+        .then(() => {
+          Storage.get(metadataPath, storageOptions)
+            .then((response) => {
+              const fr = new FileReader();
+              fr.onload = (evt) => {
+                const json = JSON.parse(evt.target.result);
+                const fixedResults = json.results.map((result) => {
+                  const fullImageUrl = `${appState.dataConfig.imageryBaseURL}/${result.imageURL}`;
+                  const fullThumbUrl = `${appState.dataConfig.thumbnailsBaseURLs}/${result.thumbnailURL}`;
+                  const fixedResult = {
+                    ...result,
+                    imageURL: fullImageUrl,
+                    thumbnailURL: fullThumbUrl,
+                  };
+                  // The ppp results json starts the rank at 0, but the pdfs start the rank at 1,
+                  // so we need to add 1 to the pppRank in the JSON to get the same rank in the UI
+                  // as is displayed in the pdf.
+                  if (searchType === "ppp") {
+                    fixedResult.pppRank = result.pppRank + 1;
+                  }
+                  return fixedResult;
+                });
 
-            setMatchMeta({ ...json, results: fixedResults });
-            setLoading(false);
-          };
-          fr.readAsText(response.Body);
-        }).catch((e) => {
-          if (e.response.status === 404) {
-            message.error("No results were found for the provided ID.");
-          }
-          else {
-            message.error("Unable to load matches from the server");
-          }
+                setMatchMeta({ ...json, results: fixedResults });
+                setLoading(false);
+              };
+              fr.readAsText(response.Body);
+            })
+            .catch((e) => {
+              if (e.response.status === 404) {
+                message.error("No results were found for the provided ID.");
+              } else {
+                message.error("Unable to load matches from the server");
+              }
+              setLoading(false);
+            });
+        })
+        .catch(() => {
+          message.error("Unable to load matches from the server");
           setLoading(false);
         });
-      }).catch(() => {
-        message.error("Unable to load matches from the server");
-        setLoading(false);
-      });
     }
 
     if ("prefixes" in appState.dataConfig) {
       getMatches();
     }
-  }, [matchId, appState.dataConfig, appState.dataVersion, searchResult, searchType, searchTerm]);
+  }, [
+    matchId,
+    appState.dataConfig,
+    appState.dataVersion,
+    searchResult,
+    searchType,
+    searchTerm,
+  ]);
 
   if (isLoading) {
     return (
@@ -96,25 +97,44 @@ export default function MatchesLoader({ searchResult, searchType }) {
   }
 
   const matchInput = searchResult.results?.filter(
-    result => result.id === matchId
+    (result) => result.id === matchId
   )[0];
 
   if (matchInput) {
     matchInput.precomputed = true;
 
-    const matches = matchMeta && !isLoading ? (
-      <Matches
-        input={matchInput}
-        searchType={searchType}
-        matches={matchMeta}
-        precomputed
-      />
-    ) : (
-      <p>No matches found</p>
-    );
+    const matches =
+      matchMeta && !isLoading ? (
+        <Matches
+          input={matchInput}
+          searchType={searchType}
+          matches={matchMeta}
+          precomputed
+        />
+      ) : (
+        <p>No matches Found</p>
+      );
 
     return (
       <>
+        <div className="searchTabs">
+          <Link
+            className={searchType === "ppp" ? "" : "activeSearch"}
+            to={`/search/skeletons/${searchTerm}/matches/${matchId}`}
+          >
+            Color Depth Search Results
+          </Link>
+          {matchMeta ? (
+            <Link
+              className={searchType === "ppp" ? "activeSearch" : ""}
+              to={`/search/ppp/${searchTerm}/matches/${matchId}`}
+            >
+              PatchPerPixMatch Results
+            </Link>
+          ) : (
+            ""
+          )}
+        </div>
         <h3>{searchType === "ppp" ? "PPPM" : "CDM"} Input Image</h3>
         <SearchSummary type={searchType} input={matchInput} />
         <MatchesProvider>{matches}</MatchesProvider>
@@ -128,10 +148,9 @@ export default function MatchesLoader({ searchResult, searchType }) {
       <p>Match results not found...</p>
     </>
   );
-
 }
 
 MatchesLoader.propTypes = {
   searchResult: PropTypes.object.isRequired,
-  searchType: PropTypes.string.isRequired
+  searchType: PropTypes.string.isRequired,
 };
