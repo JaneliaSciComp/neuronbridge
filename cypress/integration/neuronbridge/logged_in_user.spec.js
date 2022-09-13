3b7ebd60220db74a30178ec167b9e7e10ca0f55b
Jody Clements <jody@neomorphic.co.uk>
2022-09-13 09:46:01 -0400

    Updates logged in user tests to cover more search pages

--- cypress/integration/neuronbridge/logged_in_user.spec.js
+++ cypress/integration/neuronbridge/logged_in_user.spec.js
@@ -8,42 +8,47 @@ describe("logged in neuronbridge user", () => {
     cy.wait(2000);
   });
 
-  it("can load the home page and see the search input", () => {
+  it("can see pages only logged in users should see", () => {
     cy.findByText("Welcome to NeuronBridge", {
       timeout: 8000
     });
     cy.findByLabelText("Search", { timeout: 8000 });
-  });
-
-  it("can see the about page", () => {
-    cy.visit("/about");
+    cy.findByText("About").click();
     cy.findByText("About NeuronBridge", { timeout: 4000 });
-  });
-  it("can see the help page", () => {
-    cy.visit("/help");
+    cy.findByText("Help").click();
     cy.findByText("Obtaining the raw data");
   });
-  it("can't see the signup page", () => {
+  it("can't see the signup and login page", () => {
     // should get the homepage instead
     cy.visit("/signup");
     cy.findByText("Welcome to NeuronBridge", { timeout: 4000 });
-  });
-
-  it("can't see the login page", () => {
     cy.visit("/login");
     // should get the homepage instead
     cy.findByText("Welcome to NeuronBridge", { timeout: 4000 });
   });
 
-  it("can see the search pages", () => {
+  it("can see the search results pages", () => {
     // should see search results.
     cy.visit("/search");
     cy.findByText(/search help/i, { timeout: 4000 });
     cy.visit("/search?q=1537331894")
-    cy.findByLabelText(/color depth search results/i, { timeout: 4000 });
-    cy.visit("/search/skeletons/1537331894/matches/2945073141049430027");
-    cy.findByLabelText("Search", { timeout: 4000 });
-    cy.visit("/search/lines/LH173/matches/2711777430657302539");
-    cy.findByText(/input image/i, { timeout: 4000 });
+    cy.findByText(/female/i, { timeout: 4000 });
+    cy.findByLabelText(/color depth search results/i, { timeout: 4000 }).click();
+    cy.findByText("CDM Input Image", { timeout: 4000 });
+    cy.findByText(/r20d07/i, { timeout: 4000 }).click();
+    cy.findByText("Input Image");
+    cy.findByText(/Match 2 of 300/i);
+    cy.findByText(/Channel/i);
+    cy.findByText(/Mounting Protocol/i);
+    cy.findByText(/Magnification/i);
+    cy.findByText(/40x/i);
+    cy.findByText(/cite this match/i, { timeout: 4000 }).click();
+    cy.findByText(/Scheffer, et al., 2020/i);
+    cy.findByText(/download 3d files/i, { timeout: 4000 }).click();
+    cy.findByText(/\d+\.swc/i);
+    // cy.findAllByLabelText(/close/i, { timeout: 4000 })[0].click();
+    // cy.findByLabelText(/back to all results/i, { timeout: 4000 }).click();
+    // cy.findByLabelText(/patchperpixmatch results/i, { timeout: 4000 }).click();
+    // cy.findByText(/neuron type \/ instance/i);
   });
 });
