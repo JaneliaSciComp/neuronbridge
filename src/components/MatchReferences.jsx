76b9d58646bc63729d5213f2a9c37a195822f341
Jody Clements <jody@neomorphic.co.uk>
2022-09-22 10:29:47 -0400

    Updates MatchReferences lookup to use new publishedNames
    We are no longer relying on a translation table to convert the
    libraryName to a dataset name when looking up publications that
    reference an EM bodyid. Instead, the publishedName now includes the data
    set and version concatenated to the body id, eg:
    hembrain:v1.2.1:1234567. This id can be passed as is to the doi lookup
    function, removing any potential errors in translation.

--- src/components/MatchReferences.jsx
+++ src/components/MatchReferences.jsx
@@ -1,39 +1,51 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Auth, API } from "aws-amplify";
-import { AppContext } from "../containers/AppContext";
 
 export default function MatchReferences({ library, publishedName }) {
-  const { appState } = useContext(AppContext);
   const [publication, setPublication] = useState();
 
   useEffect(() => {
-    if (appState?.dataConfig?.datasets) {
-      Auth.currentCredentials().then(() => {
-        const queryString = library && appState.dataConfig.datasets[library]
-          ? `${appState.dataConfig.datasets[library]}#${publishedName}`
-          : publishedName;
-
-        API.get("SearchAPI", "/publishing_dois", {
-          queryStringParameters: {
-            q: queryString,
-          },
+    Auth.currentCredentials().then(() => {
+      API.get("SearchAPI", "/publishing_dois", {
+        queryStringParameters: {
+          q: publishedName,
+        },
+      })
+        .then((papers) => {
+          setPublication(papers);
         })
-          .then((papers) => {
-            setPublication(papers);
-          })
-          .catch(() => {
-            setPublication(null);
-          });
-      });
-    }
-  }, [publishedName, library, appState.dataConfig.datasets]);
+        .catch(() => {
+          setPublication(null);
+        });
+    });
+  }, [publishedName, library]);
 
   if (!publication) {
-    return <span>loading...</span>;
+    return (
+      <ul>
+        <li>loading...</li>
+      </ul>
+    );
   }
 
-  return <a href={publication.doi}>{publication.citation}</a>;
+  if (!publication.doi) {
+    return (
+      <ul>
+        <li>Not found</li>
+      </ul>
+    );
+  }
+
+  return (
+    <ul>
+      {publication.doi.map((entry) => (
+        <li>
+          <a href={entry.link}>{entry.citation}</a>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 MatchReferences.propTypes = {
