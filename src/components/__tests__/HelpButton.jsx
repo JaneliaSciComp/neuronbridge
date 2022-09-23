5a2a5be112c61bd8f546cfadc64842993fc98c92
Jody Clements <jody@neomorphic.co.uk>
2022-09-23 15:26:00 -0400

    Adds tests to make sure HelpButton is working

--- src/components/__tests__/HelpButton.jsx
+++ src/components/__tests__/HelpButton.jsx
@@ -1,8 +1,10 @@
 import "jest-axe/extend-expect";
-import React from "react";
-import { render } from "@testing-library/react";
+import React, { useContext } from "react";
+import { render, fireEvent } from "@testing-library/react";
 import { axe } from "jest-axe";
 import HelpButton from "../Help/HelpButton";
+import { AppContext } from "../../containers/AppContext";
+
 
 describe("HelpButton: unit tests", () => {
   it("is accessible", async () => {
@@ -14,5 +16,28 @@ describe("HelpButton: unit tests", () => {
     rerender(<HelpButton text="Matches Help" target="EmtoLmMatches" />);
     const updated = await axe(container);
     expect(updated).toHaveNoViolations();
+  });
+
+  const setStateMock = jest.fn();
+  it("updates AppContext when pressed", async () => {
+    const { container, getByText } = render(
+      <AppContext.Provider
+        value={{
+          appState: {
+            showHelp: false,
+            helpTarget: null
+          },
+          setAppState: setStateMock,
+        }}
+      >
+        <HelpButton target="EmtoLmMatches" />
+      </AppContext.Provider>
+    );
+    expect(getByText(/\?/i));
+    const button = container.querySelector('button');
+    fireEvent.click(button);
+    expect(setStateMock.mock.calls.length).toBe(1);
+    expect(setStateMock.mock.calls[0][0].helpTarget).toBe('EmtoLmMatches');
+    expect(setStateMock.mock.calls[0][0].showHelp).toBe(true);
   });
 });
