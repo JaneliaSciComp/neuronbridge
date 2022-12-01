2d3049e307bffb838d1ff3aa081597b8e0aa6d7e
Konrad Rokicki <rokickik@janelia.hhmi.org>
2022-12-01 13:33:06 -0500

    added Philip, additional descriptions, and changed layout to 3 columns

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,67 +11,83 @@ export default function About() {
     <div>
       <Title>About NeuronBridge</Title>
       <Paragraph>
+        NeuronBridge is a web application for easily and rapidly finding putative matches between large data sets of neurons imaged using different modalities, 
+        namely electron microscopy (EM) and light microscopy (LM). Matches have been precomputed for all of Janelia&apos;s public EM/LM data sets, and are 
+        quick to look up by identifier. You can also upload your own data and match it against these public data sets. 
+      </Paragraph>
+      <Paragraph>
         A preprint describing this software in detail is <a href="https://doi.org/10.1101/2022.07.20.500311">available on bioRxiv</a>.
       </Paragraph>
       <Paragraph>
         <Link to="/usage">View Usage Terms</Link>
       </Paragraph>
-      <Paragraph>
-        <Title level={3}>Release notes:</Title>
-        <ul>
-          {Object.keys(config.releasenotes).map(name => (
-            <li key={name}>
-              <Link to={{ pathname: `/releasenotes/${name}` }}>
-                {config.releasenotes[name].title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </Paragraph>
-      <Title level={3}>Site Feedback Survey:</Title>
-      <Paragraph>
-        Please take a moment to fill out our{" "}
-        <a href="https://forms.gle/tJa6GAwEjs7fykhG7">site feedback survey</a>.
-      </Paragraph>
-      <Row>
-        <Col sm={12}>
-          <Title>Source Code</Title>
-          <p>You can find all of the code for this website and the supporting infrastructure in our <a href="https://github.com/JaneliaSciComp/neuronbridge">GitHub repositories</a>.</p>
-          <Title>Team</Title>
-          <Title level={3}>Scientific Advisors:</Title>
-          <ul>
-            <li>Geoffrey Meissner</li>
-            <li>Wyatt Korff</li>
-            <li>Gudrun Ihrke</li>
-          </ul>
 
-          <Title level={3}>Data Scientists:</Title>
+      <Row gutter={20}>
+        
+        <Col sm={7}>
+          <Title level={2}>Team</Title>
+          <Title level={3}>Scientific Advisors</Title>
+          <ul>
+            <li>Geoffrey W. Meissner</li>
+            <li>Gudrun Ihrke</li>
+            <li>Wyatt Korff</li>
+            <li>Gerald M. Rubin</li>
+          </ul>
+          <Title level={3}>Data Scientists</Title>
           <ul>
             <li>Hideo Otsuna</li>
           </ul>
-
-          <Title level={3}>Software Developers:</Title>
+          <Title level={3}>Software Developers</Title>
           <ul>
             <li>Jody Clements</li>
             <li>Cristian Goina</li>
-            <li>Donald Olbris</li>
-            <li>Rob Svirskas</li>
+            <li>Phillip M. Hubbard</li>
             <li>Takashi Kawase</li>
+            <li>Donald J. Olbris</li>
+            <li>Robert R. Svirskas</li>
             <li>Konrad Rokicki</li>
           </ul>
-          <Title level={3}>Logo Design:</Title>
+          <Title level={3}>Logo Design</Title>
           <ul>
             <li>Brianna Yarbrough</li>
             <li>Yisheng He</li>
           </ul>
-          <Title level={3}>Alumni:</Title>
+          <Title level={3}>Alumni</Title>
           <ul>
             <li>Antje Kazimiers</li>
           </ul>
         </Col>
-        <Col sm={12}>
-          <Title>References</Title>
+
+        <Col sm={7}>
+          <Title level={2}>References</Title>
           <References />
+        </Col>
+
+        <Col sm={10}>
+        
+          <Title level={2}>Release Notes</Title>
+          <ul>
+            {Object.keys(config.releasenotes).map(name => (
+              <li key={name}>
+                <Link to={{ pathname: `/releasenotes/${name}` }}>
+                  {config.releasenotes[name].title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        
+          <Title level={2}>Site Feedback Survey</Title>
+          <Paragraph>
+            Please take a moment to fill out our{" "}
+            <a href="https://forms.gle/tJa6GAwEjs7fykhG7">site feedback survey</a>.
+          </Paragraph>
+
+          <Title level={2}>Source Code</Title>
+          <Paragraph>
+            You can find all of the code for this website and the supporting infrastructure in our{" "}
+            <a href="https://github.com/JaneliaSciComp/neuronbridge">GitHub repositories</a>.
+          </Paragraph>
+
         </Col>
       </Row>
     </div>
