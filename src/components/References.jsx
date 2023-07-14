6c28701c410430a25bb13950ba91c596ea24b8f6
Rob Svirskas <robsvi@gmail.com>
2023-07-14 11:01:55 -0400

    Added latest Split-GAL4 references to src/components/References.jsx

--- src/components/References.jsx
+++ src/components/References.jsx
@@ -45,41 +45,95 @@ export default function References() {
         <a href="https://doi.org/10.1101/198648"> Tirian and Dickson, 2017; for VT hemidrivers</a>
       </dd>
 
-      <dt>Split-GAL4 Lines</dt>
-      <dd>
-        <a href="https://doi.org/10.1016/j.neuron.2013.05.024">Tuthill, et al., 2013;</a>
-      </dd>
-      <dd>
-        <a href="https://doi.org/10.7554/eLife.04577">Aso, et al., 2014;</a>
-      </dd>
-      <dd>
-        <a href="https://doi.org/10.7554/eLife.16135">Aso & Rubin, 2016;</a>
-      </dd>
-      <dd>
-        <a href="https://doi.org/10.7554/eLife.21022">Wu, et al., 2016;</a>
-      </dd>
-      <dd>
-        <a href="https://doi.org/10.1038/nature24626">Klapoetke, et al., 2017;</a>
-      </dd>
-      <dd>
-        <a href="https://doi.org/10.1016/j.cell.2017.06.032">Robie, et al., 2017;</a>
-      </dd>
-      <dd>
-        <a href="https://doi.org/10.7554/eLife.34272">Namiki, et al., 2018;</a>
-      </dd>
-      <dd>
-        <a href="https://doi.org/10.1002/cne.24512">Wolff & Rubin, 2018;</a>
-      </dd>
-      <dd>
-        <a href="https://doi.org/10.7554/eLife.43079">Dolan, et al., 2019;</a>
-      </dd>
-      <dd>
-        <a href="https://doi.org/10.1038/s41586-020-2055-9">Wang, et al., 2020</a>
-      </dd>
-
       <dt>EM-LM unification template</dt>
       <dd>
         <a href="https://doi.org/10.1371/journal.pone.0236495">Bogovic et al., 2020</a>
+      </dd>
+
+      <dt>Split-GAL4 Lines</dt>
+      <dd>
+        <a href="https://doi.org/10.1016/j.neuron.2013.05.024">Tuthill et al., 2013</a>
+      </dd>
+      <dd>
+        <a href="https://doi.org/10.7554/eLife.04577">Aso et al., 2014</a>
+      </dd>
+      <dd>
+        <a href="https://doi.org/10.7554/eLife.21022">Wu et al., 2016</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.7554/eLife.08758'>Hampel et al., 2015</a>
+      </dd>
+      <dd>
+        <a href="https://doi.org/10.7554/eLife.16135">Aso & Rubin, 2016</a>
+      </dd>
+      <dd>
+        <a href="https://doi.org/10.1016/j.cell.2017.06.032">Robie et al., 2017</a>
+      </dd>
+      <dd>
+        <a href="https://doi.org/10.1038/nature24626">Klapoetke et al., 2017</a>
+      </dd>
+      <dd>
+        <a href="https://doi.org/10.7554/eLife.34272">Namiki et al., 2018</a>
+      </dd>
+      <dd>
+        <a href="https://doi.org/10.1002/cne.24512">Wolff & Rubin 2018</a>
+      </dd>
+      <dd>
+        <a href="https://doi.org/10.7554/eLife.43079">Dolan et al., 2019</a>
+      </dd>
+      <dd>
+        <a href="https://doi.org/10.1126/science.aau8302">Gao et al., 2019</a>
+      </dd>
+      <dd>
+        <a href="https://doi.org/10.1038/s41586-020-2055-9">Wang et al., 2020</a>
+      </dd> 
+      <dd>
+        <a href='https://doi.org/10.7554/elife.57685'>Morimoto et al., 2020</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.7554/eLife.50901'>Davis et al., 2020</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.1016/j.cub.2020.07.083'>Wang et al., 2020</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.1016/j.neuron.2020.08.006'>Turner-Evans et al., 2020</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.1038/s41586-020-2972-7'>Wang et al., 2020</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.7554/eLife.58942'>Schretter et al., 2020</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.1038/s41467-020-19936-x'>Feng et al., 2020</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.1371/journal.pone.0236495'>Bogovic et al., 2020</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.1101/2021.07.23.453511'>Mais et al., 2021</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.7554/eLife.71679'>Sterne et al., 2021</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.1016/j.cub.2022.01.008'>Namiki et al., 2022</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.7554/eLife.71858'>Kind et al., 2021</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.7554/eLife.66039'>Hulse et al., 2021</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.1101/2021.11.03.467132'>Longden et al., 2021</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.1016/j.neuron.2022.02.013'>Klapoetke et al., 2022</a>
+      </dd>
+      <dd>
+        <a href='https://doi.org/10.1016/j.cub.2022.06.019'>Baker et al., 2022</a>
       </dd>
 
     </dl>
