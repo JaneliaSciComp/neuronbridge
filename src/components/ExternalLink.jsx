be04128a31593ea63f23775d72749ecce996ece1
Jody Clements <jody@neomorphic.co.uk>
2024-05-24 14:54:12 -0400

    feat(ExternalLinks): Adds FLyWire link for new dataset

--- src/components/ExternalLink.jsx
+++ src/components/ExternalLink.jsx
@@ -9,7 +9,42 @@ const mcfoUrl =
   "http://gen1mcfo.janelia.org/cgi-bin/view_gen1mcfo_imagery.cgi?slidecode=<NAME>";
 // https://neuprint.janelia.org/view?dataset=hemibrain:v1.1&bodyid=12345678
 
-const vfbUrl = "http://virtualflybrain.org/xref/neuronbridge/<NAME>";
+function VFBLink({ name }) {
+  const vfbUrl = "http://virtualflybrain.org/xref/neuronbridge/<NAME>";
+
+  return (
+    <a href={vfbUrl.replace(/<NAME>/, name)}>
+      Virtual Fly Brain{" "}
+      <FontAwesomeIcon icon={faExternalLink} size="xs" transform="up-2" />
+    </a>
+  );
+}
+
+VFBLink.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+function FlyWireLink({ tag }) {
+  const [, version, id] = tag.split(":");
+
+  const flyWireUrl =
+    "https://codex.flywire.ai/app/cell_details?data_version=<VERSION>&root_id=<ID>";
+
+  return (
+    <a
+      href={flyWireUrl
+        .replace(/<ID>/, id)
+        .replace(/<VERSION>/, version.replace(/^v/, ""))}
+    >
+      FlyWire{" "}
+      <FontAwesomeIcon icon={faExternalLink} size="xs" transform="up-2" />
+    </a>
+  );
+}
+
+FlyWireLink.propTypes = {
+  tag: PropTypes.string.isRequired,
+};
 
 export default function ExternalLink({ id, isLM, library, publishedName }) {
   if (isLM) {
@@ -33,14 +68,7 @@ export default function ExternalLink({ id, isLM, library, publishedName }) {
         ) : (
           <>
             <br />
-            <a href={vfbUrl.replace(/<NAME>/, publishedName)}>
-              Virtual Fly Brain{" "}
-              <FontAwesomeIcon
-                icon={faExternalLink}
-                size="xs"
-                transform="up-2"
-              />
-            </a>
+            <VFBLink name={publishedName} />
           </>
         )}
       </>
@@ -58,21 +86,31 @@ export default function ExternalLink({ id, isLM, library, publishedName }) {
   // version number, but the pre-release site doesn't have a version number.
   // Also, the pre-release data on neuprint was called vnc and not manc, so the
   // dataset name needs to be converted as well.
-  if (process.env.REACT_APP_LEVEL && process.env.REACT_APP_LEVEL.match(/pre$/)) {
-    dataset = dataset.split(':').shift();
-    if (dataset === 'manc') {
-      dataset = 'vnc';
+  if (
+    process.env.REACT_APP_LEVEL &&
+    process.env.REACT_APP_LEVEL.match(/pre$/)
+  ) {
+    dataset = dataset.split(":").shift();
+    if (dataset === "manc") {
+      dataset = "vnc";
     }
   }
 
   const emUrl =
-  process.env.REACT_APP_LEVEL && process.env.REACT_APP_LEVEL.match(/pre$/)
-  ? "https://neuprint-pre.janelia.org/view?dataset=<DATASET>&bodyid=<NAME>"
-    : "https://neuprint.janelia.org/view?dataset=<DATASET>&bodyid=<NAME>";
+    process.env.REACT_APP_LEVEL && process.env.REACT_APP_LEVEL.match(/pre$/)
+      ? "https://neuprint-pre.janelia.org/view?dataset=<DATASET>&bodyid=<NAME>"
+      : "https://neuprint.janelia.org/view?dataset=<DATASET>&bodyid=<NAME>";
 
   const finalEMUrl = emUrl
-    .replace(/<NAME>/, id.split(':').slice(-1))
+    .replace(/<NAME>/, id.split(":").slice(-1))
     .replace(/<DATASET>/, dataset);
+
+  const secondaryLink = library.match(/flywire_fafb/i) ? (
+    <FlyWireLink tag={id} />
+  ) : (
+    <VFBLink name={id} />
+  );
+
   return (
     <>
       <a href={finalEMUrl}>
@@ -85,14 +123,7 @@ export default function ExternalLink({ id, isLM, library, publishedName }) {
       ) : (
         <>
           <br />
-          <a href={vfbUrl.replace(/<NAME>/, id)}>
-            Virtual Fly Brain{" "}
-            <FontAwesomeIcon
-              icon={faExternalLink}
-              size="xs"
-              transform="up-2"
-            />
-          </a>
+          {secondaryLink}
         </>
       )}
     </>
@@ -103,11 +134,11 @@ ExternalLink.propTypes = {
   id: PropTypes.string.isRequired,
   publishedName: PropTypes.string,
   library: PropTypes.string,
-  isLM: PropTypes.bool
+  isLM: PropTypes.bool,
 };
 
 ExternalLink.defaultProps = {
   isLM: true,
   publishedName: "",
-  library: "flylight_splitgal4_drivers"
+  library: "flylight_splitgal4_drivers",
 };
