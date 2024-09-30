86ffe6f286921ae03b52841faa3ee5294d61fd5e
Jody Clements <jody@neomorphic.co.uk>
2024-09-30 14:41:46 -0400

    feat: Converts searchType to image collections
    The custom search uploads will be run against image collections in the
    future, so the upload form has been modified to provide a list of the
    collections to choose from as opposed to choosing either EM or LM
    imagery. This also adds a check to convert any older searches that have
    been re run, so that they are using the correct collection.

--- src/components/ColorDepthSearchParameters.jsx
+++ src/components/ColorDepthSearchParameters.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
+import propTypes from "prop-types";
 import { Typography, Form, Select, Input, Checkbox } from "antd";
+import { AppContext } from "../containers/AppContext";
 
 const { Title } = Typography;
 const { Option } = Select;
 
-export default function ColorDepthSearchParameters() {
+// TODO: load the image collections from the appcontext and populate the select options
+// instead of hardcoding them.
+export default function ColorDepthSearchParameters({ searchMeta }) {
+  const { appState } = useContext(AppContext);
+
+  const collections = [];
+
+  Object.values(appState.dataConfig.stores).forEach((store) => {
+    // if the value in the anatomical area does not match to a case
+    // insensitive substring of the store anatomical area, then skip it.
+    if (
+      searchMeta.anatomicalRegion.toLowerCase() !==
+      store.anatomicalArea.toLowerCase()
+    ) {
+      return;
+    }
+    const { lmLibraries, emLibraries } = store.customSearch;
+    lmLibraries.forEach((library) => {
+      collections.push(
+        <Option key={library.name} value={library.name}>
+          {library.name} ({library.count})
+        </Option>,
+      );
+    });
+    emLibraries.forEach((library) => {
+      collections.push(
+        <Option key={library.name} value={library.name}>
+          {library.name} ({library.count})
+        </Option>,
+      );
+    });
+  });
+
   return (
     <div>
       <Title level={3}>Set the search parameters</Title>
       <Form.Item
-        label="Target Image Library"
+        label="Target Image Collection"
         name="searchType"
         rules={[{ required: true, message: "Please choose a search type!" }]}
       >
-        <Select>
-          <Option value="em2lm">Light Microscopy Libraries</Option>
-          <Option value="lm2em">Electron Microscopy Libraries</Option>
-        </Select>
+        <Select>{collections}</Select>
       </Form.Item>
       <Form.Item
         extra="Values between 0 - 255. Intensity values in the mask image which are below this cutoff are ignored."
@@ -68,3 +99,7 @@ export default function ColorDepthSearchParameters() {
     </div>
   );
 }
+
+ColorDepthSearchParameters.propTypes = {
+  searchMeta: propTypes.object.isRequired,
+};
