901e9352217b414eb5f5c8572dfe60f20d8bed45
Jody Clements <jody@neomorphic.co.uk>
2025-03-12 10:23:05 -0400

    fix: Makes curated match count bold to highlight it.
    The idea is to draw more attention to the result count, so people don't
    think there are only 2 results.

--- src/components/UnifiedSearch.jsx
+++ src/components/UnifiedSearch.jsx
@@ -461,7 +461,7 @@ export default function UnifiedSearch() {
   if (curatedResults.length > 0) {
     items.unshift({
       key: "1",
-      label: `Curated Matches of Split-GAL4 Lines to Cell Types (${curatedResults.length} items)`,
+      label: <p>Curated Matches of Split-GAL4 Lines to Cell Types <b>({curatedResults.length} items)</b></p>,
       children: curatedMatches,
       extra: (
         <HelpButton
